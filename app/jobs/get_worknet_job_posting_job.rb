8a954e45596198db62b9ce92e977ebe128d70849
godwon2095 <jsw2095@naver.com>
2023-01-20 14:15:24 +0900

    FIX: cron peroid changed 매일 오전 8시부터 19시까지 10분에 한번씩

--- app/jobs/get_worknet_job_posting_job.rb
+++ app/jobs/get_worknet_job_posting_job.rb
@@ -1,5 +1,7 @@
 class GetWorknetJobPostingJob < ApplicationJob
-  rate "10 minutes" # every 10 hours
+
+  # 매일 오전 8시부터 19시까지 10분에 한번씩 돌아감
+  cron "0 0/10 0,1,2,3,4,5,6,7,8,9,10,23 ? * * *"
   def dig
     GetWorknetJobService.call
   end
