c249a69dc5e7faccb60479edf2143e6c09d47c11
godwon2095 <jsw2095@naver.com>
2023-02-06 16:16:39 +0900

    FIX: Change function name for test

--- app/jobs/new_job_notification_job.rb
+++ app/jobs/new_job_notification_job.rb
@@ -1,5 +1,5 @@
 class NewJobNotificationJob < ApplicationJob
-  def call
+  def dig
     job_posting = JobPosting.find(event[:job_posting_id])
     NewJobNotificationService.test_call(job_posting) unless Jets.env == 'production'
     NewJobNotificationService.call(job_posting) if Jets.env == 'production'
