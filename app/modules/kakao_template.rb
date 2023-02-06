ffc71efb6edb5eafd1b142f48e7aea0c5b6cd937
godwon2095 <jsw2095@naver.com>
2023-02-06 16:39:04 +0900

    FIX: Change config iam rule for production

--- app/modules/kakao_template.rb
+++ app/modules/kakao_template.rb
@@ -2,4 +2,5 @@ module KakaoTemplate
   PROPOSAL = "proposal_02".freeze
   NEW_JOB_POSTING_VISIT = "new_job_visit".freeze
   NEW_JOB_POSTING_FACILITY = "new_job_facility".freeze
+  PERSONALIZED = "personalized_job_message".freeze
 end
