827f0c6e1c3350864a8719b05fc21d3904869198
godwon2095 <jsw2095@naver.com>
2023-02-07 15:14:56 +0900

    FIX: 맞춤일자리 템플릿 변경

--- app/modules/kakao_template.rb
+++ app/modules/kakao_template.rb
@@ -1,7 +1,7 @@
 module KakaoTemplate
   PROPOSAL = "proposal_02".freeze
-  NEW_JOB_POSTING_VISIT = "new_job_visit".freeze
+  NEW_JOB_POSTING_VISIT = "new_job_visit_1".freeze
   NEW_JOB_POSTING_FACILITY = "new_job_facility".freeze
-  PERSONALIZED = "personalized_job_message".freeze
+  PERSONALIZED = "personalized_job_message_edit".freeze
   EXTRA_BENEFIT = "extra_benefits_job_message_2".freeze
 end
