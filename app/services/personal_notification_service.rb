e6b2b04c128a0557565ca8e5346cee7c79880066
godwon2095 <jsw2095@naver.com>
2023-02-14 16:52:39 +0900

    FIX: 테스트 5시 발송 설정

--- app/services/personal_notification_service.rb
+++ app/services/personal_notification_service.rb
@@ -14,7 +14,7 @@ class PersonalNotificationService
     fail_reasons = []
     users = User.where(phone_number: %w[01097912095 01051119300 01094659404 01066121746])
     users = users.limit(3) if Jets.env == "development"
-    users = test_users(users) # if Jets.env == "staging" # WARNING 바꾸면 실제 유저에게 배포됨
+    users = test_users(users) if Jets.env == "staging" # WARNING 바꾸면 실제 유저에게 배포됨
     users.find_each do |user|
       begin
         response = send_notification(user)
@@ -100,6 +100,6 @@ class PersonalNotificationService
   end
 
   def test_users(users)
-    users.where(phone_number: %w[01097912095 01051119300 01094659404 01066121746])
+    users.where(phone_number: %w[])
   end
 end
