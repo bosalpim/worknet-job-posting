d2f1e5b41c637e199c9c4046d7fbff2427221502
hanwool95 <hanwool95@snu.ac.kr>
2023-02-22 15:02:30 +0900

    FEATURE: 네이버에 크롤링 API POST 하는 서비스 제작 및 환경변수 추가.
    top-110

---
+++ app/services/post_naver_crawling_api_service.rb
@@ -0,0 +1,31 @@
+class PostNaverCrawlingApiService
+
+  def call(url)
+    response = HTTParty.post(
+      "https://apis.naver.com/searchadvisor/crawl-request/submit.json",
+      body: JSON.dump(
+        {
+          "urls": [
+            {
+              "url": url,
+              "type": "update"
+            }
+          ]
+        }
+      ),
+      headers: {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer " + ENV['NAVER_SEARCH_API_KEY']
+      }
+    )
+
+    if response.errorCode
+      puts response.errorCode
+    end
+  end
+
+  private
+
+  attr_reader :url_object, :google_service
+end
+
