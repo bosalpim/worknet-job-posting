8a093dd19a0b263798593fee6a74496c096ed2ef
godwon2095 <jsw2095@naver.com>
2023-02-24 11:39:41 +0900

    FIX: 알림톡 발송 10% 단위로 쪼개어 부하를 줄이는 작업

--- app/jobs/extra_benefit_notification_job.rb
+++ app/jobs/extra_benefit_notification_job.rb
@@ -1,26 +1,10 @@
 class ExtraBenefitNotificationJob < ApplicationJob
-  cron "0 3 ? * THU *" # “At 12:00 on Thursday in Korean Time”
-  def send_1_percent
-    ExtraBenefitNotificationService.call(0.01, 0)
-  end
-
-  cron "10 3 ? * THU *" # “At 12:10 on Thursday in Korean Time”
-  def send_9_percent
-    ExtraBenefitNotificationService.call(0.09, 0.01)
-  end
-
-  cron "20 3 ? * THU *" # “At 12:20 on Thursday in Korean Time”
-  def send_20_percent
-    ExtraBenefitNotificationService.call(0.20, 0.10)
-  end
-
-  cron "25 3 ? * THU *" # “At 12:25 on Thursday in Korean Time”
-  def send_35_percent
-    ExtraBenefitNotificationService.call(0.35, 0.30)
-  end
-
-  cron "30 3 ? * THU *" # “At 12:30 on Thursday in Korean Time”
-  def send_last_35_percent
-    ExtraBenefitNotificationService.call(0.35, 0.65)
+  10.times do |i|
+    self.class_eval <<~CODE
+      cron "#{i * 5} 3 ? * THU *"
+      def send_step_#{i}
+        ExtraBenefitNotificationService.call(0.1, #{0.1 * i})
+      end
+    CODE
   end
 end
--- app/jobs/personal_notification_job.rb
+++ app/jobs/personal_notification_job.rb
@@ -1,26 +1,10 @@
 class PersonalNotificationJob < ApplicationJob
-  cron "0 3 ? * MON *" # “At 12:00 on Monday in Korean Time”
-  def send_1_percent
-    PersonalNotificationService.call(0.01, 0)
-  end
-
-  cron "10 3 ? * MON *" # “At 12:10 on Monday in Korean Time”
-  def send_9_percent
-    PersonalNotificationService.call(0.09, 0.01)
-  end
-
-  cron "20 3 ? * MON *" # “At 12:20 on Monday in Korean Time”
-  def send_20_percent
-    PersonalNotificationService.call(0.20, 0.10)
-  end
-
-  cron "25 3 ? * MON *" # “At 12:25 on Monday in Korean Time”
-  def send_35_percent
-    PersonalNotificationService.call(0.35, 0.30)
-  end
-
-  cron "30 3 ? * MON *" # “At 12:30 on Monday in Korean Time”
-  def send_last_35_percent
-    PersonalNotificationService.call(0.35, 0.65)
+  10.times do |i|
+    self.class_eval <<~CODE
+      cron "#{i * 5} 3 ? * MON *"
+      def send_step_#{i}
+        PersonalNotificationService.call(0.1, #{0.1 * i})
+      end
+    CODE
   end
 end
