5979b53f163645f0057fb4f916361cea58f6ce88
godwon2095 <jsw2095@naver.com>
2023-02-27 14:03:35 +0900

    FIX: change time

--- app/jobs/personal_notification_job.rb
+++ app/jobs/personal_notification_job.rb
@@ -8,46 +8,46 @@ class PersonalNotificationJob < ApplicationJob
 
   cron "5 3 ? * MON *"
   def send_step_1
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.1)
   end
 
   cron "10 3 ? * MON *"
   def send_step_2
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.2)
   end
 
   cron "15 3 ? * MON *"
   def send_step_3
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.3)
   end
 
   cron "20 3 ? * MON *"
   def send_step_4
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.4)
   end
 
-  cron "55 4 ? * MON *"
+  cron "15 5 ? * MON *"
   def send_step_5
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.5)
   end
 
-  cron "5 5 ? * TUE *"
+  cron "25 5 ? * MON *"
   def send_step_6
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.6)
   end
 
-  cron "15 5 ? * TUE *"
+  cron "35 5 ? * MON *"
   def send_step_7
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.7)
   end
 
-  cron "25 5 ? * TUE *"
+  cron "45 5 ? * MON *"
   def send_step_8
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.8)
   end
 
-  cron "35 5 ? * TUE *"
+  cron "55 5 ? * MON *"
   def send_step_9
-    PersonalNotificationService.call(0.1, 0)
+    PersonalNotificationService.call(0.1, 0.9)
   end
 end
