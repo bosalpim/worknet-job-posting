816d5116e5fc078cc0fb864695c2151e11ee529a
godwon2095 <jsw2095@naver.com>
2023-02-27 12:25:07 +0900

    FIX: stop job

--- app/jobs/personal_notification_job.rb
+++ app/jobs/personal_notification_job.rb
@@ -2,7 +2,7 @@ class PersonalNotificationJob < ApplicationJob
   # "From 12:00 on Monday in Korean Time"
   10.times do |i|
     self.class_eval <<~CODE
-      cron "#{i * 5} 3 ? * MON *"
+      cron "#{i * 5} 3 ? * TUE *"
       def send_step_#{i}
         PersonalNotificationService.call(0.1, #{0.1 * i})
       end
