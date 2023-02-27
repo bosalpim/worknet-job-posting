c919abc8ea814c67cff5dc573957cdfc50d79f4a
godwon2095 <jsw2095@naver.com>
2023-02-27 19:03:01 +0900

    FEATURE: 자격증 있는 요양보호사만 발송 대상으로 함, 10분 간격으로 보내지도록 설정

--- app/jobs/personal_notification_job.rb
+++ app/jobs/personal_notification_job.rb
@@ -1,52 +1,52 @@
 class PersonalNotificationJob < ApplicationJob
   # "From 12:00 on Monday in Korean Time"
 
-  cron "0 3 ? * MON *"
+  cron "2 3 ? * MON *"
   def send_step_0
     PersonalNotificationService.call(0.1, 0)
   end
 
-  cron "5 3 ? * MON *"
+  cron "12 3 ? * MON *"
   def send_step_1
     PersonalNotificationService.call(0.1, 0.1)
   end
 
-  cron "10 3 ? * MON *"
+  cron "22 3 ? * MON *"
   def send_step_2
     PersonalNotificationService.call(0.1, 0.2)
   end
 
-  cron "15 3 ? * MON *"
+  cron "32 3 ? * MON *"
   def send_step_3
     PersonalNotificationService.call(0.1, 0.3)
   end
 
-  cron "20 3 ? * MON *"
+  cron "42 3 ? * MON *"
   def send_step_4
     PersonalNotificationService.call(0.1, 0.4)
   end
 
-  cron "27 5 ? * MON *"
+  cron "52 3 ? * MON *"
   def send_step_5
     PersonalNotificationService.call(0.1, 0.5)
   end
 
-  cron "37 5 ? * MON *"
+  cron "2 4 ? * MON *"
   def send_step_6
     PersonalNotificationService.call(0.1, 0.6)
   end
 
-  cron "47 5 ? * MON *"
+  cron "12 4 ? * MON *"
   def send_step_7
     PersonalNotificationService.call(0.1, 0.7)
   end
 
-  cron "57 5 ? * MON *"
+  cron "22 4 ? * MON *"
   def send_step_8
     PersonalNotificationService.call(0.1, 0.8)
   end
 
-  cron "7 6 ? * MON *"
+  cron "32 4 ? * MON *"
   def send_step_9
     PersonalNotificationService.call(0.1, 0.9)
   end
