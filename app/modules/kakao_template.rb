afbb7713d528309e86febb4bc9669050176c62e4
JeongHaMin <wjdgkals74@gmail.com>
2023-04-18 19:26:55 +0900

    FEAT : 유저 설문 알림톡 변경 (#54)
    * FEAT : 유저 설문 알림톡 변경
    
    * FEAT : 신규 일자리 알림톡 개선
    
    * FEAT : 기관 설문 만족도 조사 수정 반영

--- app/modules/kakao_template.rb
+++ app/modules/kakao_template.rb
@@ -1,13 +1,13 @@
 module KakaoTemplate
   PROPOSAL = "proposal_response".freeze
-  NEW_JOB_POSTING_VISIT = "new_job_visit_2(edit)".freeze
-  NEW_JOB_POSTING_FACILITY = "new_job_facility_2(edit)".freeze
+  NEW_JOB_POSTING_VISIT = "new_job_visit(hot_deal)".freeze
+  NEW_JOB_POSTING_FACILITY = "new_job_facility(hot_deal)".freeze
   PERSONALIZED = "personalized_job_message_edit3".freeze # done
   EXTRA_BENEFIT = "extra_benefits_job_message_4".freeze
   PROPOSAL_ACCEPTED = "proposal_accepted3".freeze
   PROPOSAL_REJECTED = "proposal_refused".freeze
-  SATISFACTION_SURVEY = "satisfaction_survey_interview2".freeze # 기관 채용만족도 조사 알림톡
-  USER_SATISFACTION_SURVEY = "user_satisfaction_survey".freeze # 요양보호사 채용만족도 조사 알림톡
+  SATISFACTION_SURVEY = "business_satisfaction_survey".freeze # 기관 채용만족도 조사 알림톡
+  USER_SATISFACTION_SURVEY = "user_satisfaction_survey(edit)".freeze # 요양보호사 채용만족도 조사 알림톡
   USER_CALL_REMINDER = "proposal_call_remind_user1".freeze # 기관이 요양보호사에게 전화했을 때 안받은 경우
   BUSINESS_CALL_REMINDER = "posting_call_reminder_biz1".freeze # 요보사가 기관에게 전화했을 때 안받은 경우
   CALL_REQUEST_ALARM = "call_request_alarm".freeze # 요양보호사가 기관에 지원했을 때, 기관에게 지원 안내를 하는 알림톡입니다.
