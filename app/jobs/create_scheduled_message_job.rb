ccd2cbfa8a118c95fd63901f52dcc7bf345fcac8
JeongHaMin <wjdgkals74@gmail.com>
2023-04-20 15:49:19 +0900

    FIX : 크론잡 시간 변경

--- app/jobs/create_scheduled_message_job.rb
+++ app/jobs/create_scheduled_message_job.rb
@@ -2,12 +2,12 @@ class CreateScheduledMessageJob < ApplicationJob
   # "From 12:00 on Monday in Korean Time"
 
   cron "0 17 ? * SUN *"
-  def create_extra_benefit_notification_message
+  def create_personal_notification_message
     CreatePersonalNotificationMessageService.call
   end
 
   cron "0 17 ? * WED *"
-  def create_personal_notification_message
+  def create_extra_benefit_notification_message
     CreateExtraBenefitNotificationMessageService.call
   end
 end
