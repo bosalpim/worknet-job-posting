af0f702bbc38fc9495ca003e70c3e5d6b46e74f5
JeongHaMin <wjdgkals74@gmail.com>
2023-06-06 18:59:40 +0900

    FIX : 신문 메세지 중복발송 해결

--- app/jobs/create_scheduled_message_job.rb
+++ app/jobs/create_scheduled_message_job.rb
@@ -2,14 +2,12 @@ class CreateScheduledMessageJob < ApplicationJob
   # "From 12:00 on Monday in Korean Time"
 
   cron "0 20 ? * SUN *"
-  def create_personal_notification_message
+  def create_news_paper_mon_message
     CreateNewsPaperActivelyCommonMessageService.call
   end
 
-  # cron "0 20 ? * WED *" # HM : 원복
-  # 18 - 9 = 9
-  cron "20 9 ? * TUE *"
-  def create_extra_benefit_notification_message
+  cron "0 20 ? * WED *"
+  def create_news_paper_thu_message
     CreateNewsPaperActivelyCommonMessageService.call
   end
 end
--- app/services/send_created_scheduled_message_service.rb
+++ app/services/send_created_scheduled_message_service.rb
@@ -4,7 +4,7 @@ class SendCreatedScheduledMessageService
   end
 
   def call(template_id, send_type, should_send_percent, sent_percent)
-    # return if Jets.env != "production" # HM : 원복
+    return if Jets.env != "production"
 
     success_count = 0
     tms_success_count = 0
@@ -15,32 +15,35 @@ class SendCreatedScheduledMessageService
     counts = calculate_sent_and_message_count(total_count, should_send_percent, sent_percent)
     message_count = counts.dig(:message_count)
     sent_count = counts.dig(:sent_count)
+
     Jets.logger.info "Calculate Result > message_count : #{message_count}, sent_count: #{sent_count}"
-    messages = ScheduledMessage.where(scheduled_date: 1.days.ago..).where(template_id: template_id).offset(sent_count).limit(message_count)
+
+    messages = ScheduledMessage.where(scheduled_date: 1.days.ago..).where(template_id: template_id).where(is_send: false).limit(message_count)
+
     Jets.logger.info "Read Message Count > #{messages.length}"
 
     try_redundant_send_count = 0
     messages.find_each(batch_size: message_count) do |message|
       if should_send(message)
         begin
-          # response = KakaoNotificationService.call(
-          #   template_id: message.template_id,
-          #   message_type: template_id == KakaoTemplate::JOB_ALARM_ACTIVELY ? 'AI' : 'AT',
-          #   phone: Jets.env != 'production' ? '01094659404' : message.phone_number,
-          #   template_params: JSON.parse(message.content)
-          # )
+          response = KakaoNotificationService.call(
+            template_id: message.template_id,
+            message_type: template_id == KakaoTemplate::JOB_ALARM_ACTIVELY ? 'AI' : 'AT',
+            phone: Jets.env != 'production' ? '01094659404' : message.phone_number,
+            template_params: JSON.parse(message.content)
+          )
 
-          success_count += 1 # HM : 제거
-          # if response.dig("code") == "success"
-          #   if response.dig("message") == "K000"
-          #     success_count += 1
-          #   else
-          #     tms_success_count += 1
-          #   end
-          # else
-          #   fail_count += 1
-          # end
-          # fail_reasons.push(response.dig("originMessage")) if response.dig("message") != "K000"
+          if response.dig("code") == "success"
+            if response.dig("message") == "K000"
+              success_count += 1
+            else
+              tms_success_count += 1
+            end
+          else
+            fail_count += 1
+          end
+
+          fail_reasons.push(response.dig("originMessage")) if response.dig("message") != "K000"
         rescue => e
           fail_count += 1
           fail_reasons.push(e.message)
@@ -55,13 +58,14 @@ class SendCreatedScheduledMessageService
     end
 
     Jets.logger.info("전송 개수 : #{ScheduledMessage.where(is_send: true).length}, 미전송 개수 : #{ScheduledMessage.where(is_send: false).length}")
+
     KakaoNotificationResult.create!(
       send_type: send_type,
-      send_id: "#{should_send_percent * 100}%",
+      send_id: "#{(should_send_percent + sent_percent) * 100}%",
       template_id: template_id,
       success_count: success_count,
       tms_success_count: tms_success_count,
-      fail_count: try_redundant_send_count, # HM : fail_count
+      fail_count: fail_count,
       fail_reasons: fail_reasons.uniq.join(", ")
     )
   end
