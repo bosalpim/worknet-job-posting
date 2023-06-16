485ec37a81b7a08f473f73b09324ce6bf8fcccbe
JeongHaMin <wjdgkals74@gmail.com>
2023-06-16 16:07:11 +0900

    FEAT : 로깅 단순화

--- app/helpers/kakao_notification_logging_helper.rb
+++ app/helpers/kakao_notification_logging_helper.rb
@@ -1,13 +1,5 @@
 module KakaoNotificationLoggingHelper
   NOTIFICATION_EVENT_NAME = '[Action] Receive Notification'
-
-  SENDER_TYPE_CAREPARTNER = 'carepartner'
-  SENDER_TYPE_BUSINESS = 'business'
-  SENDER_TYPE_USER = 'user'
-
-  RECEIVER_TYPE_BUSINESS = 'business'
-  RECEIVER_TYPE_USER = 'user'
-
   NOTIFICATION_TYPE_KAKAO = 'kakao_notification'
   NOTIFICATION_TYPE_RESERVED = 'kakao_notification_reserved'
   NOTIFICATION_TYPE_APP_PUSH = 'app_push'
@@ -15,6 +7,7 @@ module KakaoNotificationLoggingHelper
 
   def self.get_logging_data(template_id, tem_params)
     target_public_id = tem_params.dig(:target_public_id)
+    target_public_id = tem_params.dig("target_public_id") if target_public_id.nil?
 
     return {
       "target_public_id" => target_public_id,
@@ -25,20 +18,6 @@ module KakaoNotificationLoggingHelper
       }
     }
   end
-  def self.get_news_paper_logging_data(template_id, phone)
-    target_public_id = User.find_by(phone_number: phone).public_id
-    return {
-      "target_public_id" => target_public_id,
-      "event_name" => NOTIFICATION_EVENT_NAME,
-      "properties" => {
-        "sender_type" => SENDER_TYPE_CAREPARTNER,
-        "receiver_type" => RECEIVER_TYPE_USER,
-        "template" => template_id,
-        "send_at" => Time.current + (9 * 60 * 60)
-      }
-    }
-  end
-
   def self.send_log(response, template_id, template_params)
     logging_data = get_logging_data(template_id, template_params)
     return if logging_data.nil?
@@ -47,11 +26,11 @@ module KakaoNotificationLoggingHelper
     response_message = response.dig("message")
     if response_code == "success"
       if response_message == "K000"
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_KAKAO
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_KAKAO
       elsif response_message == "R000"
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_RESERVED
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_RESERVED
       else
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_TEXT_MESSAGE
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_TEXT_MESSAGE
       end
       AmplitudeService.instance.log(logging_data["event_name"], logging_data["properties"], logging_data["target_public_id"])
     else
