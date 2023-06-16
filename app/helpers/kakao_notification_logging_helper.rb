62954b74cef1c090dd5a3075844d22bd3c34dc5c
JeongHaMin <wjdgkals74@gmail.com>
2023-06-16 17:25:22 +0900

    알림톡 전송 성공 로깅 단순화 (#114)
    * FEAT : 신규일자리 알림 로깅분리
    
    * FEAT : 로깅 단순화
    
    * FEAT : 아직 로깅 처리하지 않은 대상들 nil 반환 예외처리
    
    * FEAT : 기존 로깅함수랑 새로운 형태 함수 추가

--- app/helpers/kakao_notification_logging_helper.rb
+++ app/helpers/kakao_notification_logging_helper.rb
@@ -1,5 +1,6 @@
 module KakaoNotificationLoggingHelper
   NOTIFICATION_EVENT_NAME = '[Action] Receive Notification'
+  NOTIFICATION_EVENT_NAME2 = '[Action] Receive Notification2'
 
   SENDER_TYPE_CAREPARTNER = 'carepartner'
   SENDER_TYPE_BUSINESS = 'business'
@@ -13,64 +14,48 @@ module KakaoNotificationLoggingHelper
   NOTIFICATION_TYPE_APP_PUSH = 'app_push'
   NOTIFICATION_TYPE_TEXT_MESSAGE = 'text_message'
 
-  def self.get_logging_data(template_id, tem_params, phone)
-    case template_id
-    when KakaoTemplate::PROPOSAL
+  def self.get_logging_data2(template_id, tem_params)
+    target_public_id = tem_params.dig(:target_public_id)
+    target_public_id = tem_params.dig("target_public_id") if target_public_id.nil?
+
+    if target_public_id.nil?
       return nil
-    when KakaoTemplate::NEW_JOB_POSTING_VISIT
-      return self.get_new_job_posting_logging_data(tem_params, phone, template_id)
-    when KakaoTemplate::NEW_JOB_POSTING_FACILITY
-      return self.get_new_job_posting_logging_data(tem_params, phone, template_id)
-    when KakaoTemplate::PERSONALIZED
-      return nil
-    when KakaoTemplate::EXTRA_BENEFIT
-      return nil
-    when KakaoTemplate::PROPOSAL_ACCEPTED
-      return nil
-    when KakaoTemplate::PROPOSAL_REJECTED
-      return nil
-    when KakaoTemplate::SATISFACTION_SURVEY
-      return nil
-    when KakaoTemplate::USER_SATISFACTION_SURVEY
-      return nil
-    when KakaoTemplate::USER_CALL_REMINDER
-      return nil
-    when KakaoTemplate::BUSINESS_CALL_REMINDER
-      return nil
-    when KakaoTemplate::CALL_REQUEST_ALARM
-      return nil
-    when KakaoTemplate::BUSINESS_CALL_APPLY_USER_REMINDER
-      return nil
-    when KakaoTemplate::JOB_ALARM_ACTIVELY
-      return get_news_paper_logging_data(template_id, phone)
-    when KakaoTemplate::JOB_ALARM_COMMON
-      return nil
-    when KakaoTemplate::JOB_ALARM_OFF
-      return get_news_paper_logging_data(template_id, phone)
-    when KakaoTemplate::JOB_ALARM_WORKING
-      return get_news_paper_logging_data(template_id, phone)
-    when KakaoTemplate::GAMIFICATION_MISSION_COMPLETE
-      return nil
-    else
-      puts "WARNING: Amplitude Logging Missing else case!"
     end
-  end
-  def self.get_news_paper_logging_data(template_id, phone)
-    target_public_id = User.find_by(phone_number: phone).public_id
+
     return {
       "target_public_id" => target_public_id,
-      "event_name" => NOTIFICATION_EVENT_NAME,
+      "event_name" => NOTIFICATION_EVENT_NAME2,
       "properties" => {
-        "sender_type" => SENDER_TYPE_CAREPARTNER,
-        "receiver_type" => RECEIVER_TYPE_USER,
         "template" => template_id,
-        "send_at" => Time.current + (9 * 60 * 60)
+        "template_params" => tem_params
       }
     }
   end
 
-  def self.get_new_job_posting_logging_data(template_params, phone, template_id)
-    target_public_id = User.find_by(phone_number: phone).public_id
+  def self.get_logging_data(template_id, tem_params)
+    target_public_id = tem_params.dig(:target_public_id)
+    target_public_id = tem_params.dig("target_public_id") if target_public_id.nil?
+
+    if target_public_id.nil?
+      return nil
+    end
+
+    case template_id
+      when KakaoTemplate::NEW_JOB_POSTING_VISIT
+        return self.get_new_job_posting_logging_data(tem_params, template_id, target_public_id)
+      when KakaoTemplate::NEW_JOB_POSTING_FACILITY
+        return self.get_new_job_posting_logging_data(tem_params, template_id, target_public_id)
+      when KakaoTemplate::JOB_ALARM_ACTIVELY
+        return get_news_paper_logging_data(template_id, target_public_id)
+      when KakaoTemplate::JOB_ALARM_OFF
+        return get_news_paper_logging_data(template_id, target_public_id)
+      when KakaoTemplate::JOB_ALARM_WORKING
+        return get_news_paper_logging_data(template_id, target_public_id)
+    else
+      puts "WARNING: Amplitude Logging Missing else case!"
+    end
+  end
+  def self.get_new_job_posting_logging_data(template_params, template_id, target_public_id)
     job_posting_public_id = template_params.dig(:job_posting_public_id)
     job_posting_title = template_params.dig(:job_posting_title)
     business_name = template_params.dig(:business_name)
@@ -90,25 +75,43 @@ module KakaoNotificationLoggingHelper
     }
   end
 
-  def self.send_log(response, template_id, template_params, phone)
-    logging_data = get_logging_data(template_id, template_params, phone)
+  def self.get_news_paper_logging_data(template_id, target_public_id)
+    return {
+      "target_public_id" => target_public_id,
+      "event_name" => NOTIFICATION_EVENT_NAME,
+      "properties" => {
+        "sender_type" => SENDER_TYPE_CAREPARTNER,
+        "receiver_type" => RECEIVER_TYPE_USER,
+        "template" => template_id,
+        "send_at" => Time.current + (9 * 60 * 60)
+      }
+    }
+  end
+  def self.send_log(response, template_id, template_params)
+    logging_data = get_logging_data(template_id, template_params)
+    logging_data2 = get_logging_data2(template_id, template_params)
+
     return if logging_data.nil?
+    return if logging_data2.nil?
 
     response_code = response.dig("code")
     response_message = response.dig("message")
     if response_code == "success"
       if response_message == "K000"
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_KAKAO
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_KAKAO
+        logging_data2["properties"]["type"] = NOTIFICATION_TYPE_KAKAO
       elsif response_message == "R000"
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_RESERVED
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_RESERVED
+        logging_data2["properties"]["type"] = NOTIFICATION_TYPE_RESERVED
       else
-        logging_data["properties"]["type"] = KakaoNotificationLoggingHelper::NOTIFICATION_TYPE_TEXT_MESSAGE
+        logging_data["properties"]["type"] = NOTIFICATION_TYPE_TEXT_MESSAGE
+        logging_data2["properties"]["type"] = NOTIFICATION_TYPE_TEXT_MESSAGE
       end
+
       AmplitudeService.instance.log(logging_data["event_name"], logging_data["properties"], logging_data["target_public_id"])
+      AmplitudeService.instance.log(logging_data2["event_name"], logging_data2["properties"], logging_data2["target_public_id"])
     else
       return
     end
   end
-  
-
 end
