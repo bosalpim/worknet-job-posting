09a296de7c00c86871e01d19aed849fac4024345
JeongHaMin <wjdgkals74@gmail.com>
2023-06-23 17:11:01 +0900

    FEAT : 계약서 대행 정기 발송 크론잡 추가

---
+++ app/jobs/contract_agency_job.rb
@@ -0,0 +1,16 @@
+class ContractAgencyJob < ApplicationJob
+  cron "0 5 ? * MON *"
+  def send_contract_agency1
+    ContractAgencyService.call(7)
+  end
+
+  # cron "0 5 ? * TUE *"
+  # def send_contract_agency2
+  #   ContractAgencyService.call(4)
+  # end
+
+  cron "0 5 ? * FRI *"
+  def send_contract_agency3
+    ContractAgencyService.call(3)
+  end
+end
