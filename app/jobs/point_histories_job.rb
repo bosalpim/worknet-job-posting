1f839f191824537a7edfb0c76b64baae6ece6a20
JeongHaMin <wjdgkals74@gmail.com>
2023-08-23 12:30:28 +0900

    Update point_histories_job.rb (#171)

--- app/jobs/point_histories_job.rb
+++ app/jobs/point_histories_job.rb
@@ -5,6 +5,7 @@ class PointHistoriesJob < ApplicationJob
 
   private
   def update_point_invite_history(event)
+    user = User.find_by(id: event[:user_id])
     user_id = User.find_by(id: event[:user_id]).id
     histories = InvitedHistory.where(user_id: user_id).order(created_at: 'desc')
     target_history = nil
@@ -24,7 +25,7 @@ class PointHistoriesJob < ApplicationJob
         if target_history.nil? || histories.length == 0
           # 초대 코드가 없으면 가입한 사람에게만 축하포인트 적립
           item = PointItem.find_by(item_type: 'welcome')
-          PointHistory.create!(point_item_id: item.id, user_id: current_user.id)
+          PointHistory.create!(point_item_id: item.id, user_id: user.id)
         else
           # 초대 코드가 있으면 양측에 친구초대 포인트 적립
           target_history.update!(condition_fulfilled: true)
