c2be6bae4e4c7ef36c4f0186af06542db26c5b31
jhpung <jhpung.dev@gmail.com>
2023-08-30 11:54:18 +0900

    feat: 제안 시, proposal_vn 전송

--- app/services/new_proposal_service.rb
+++ app/services/new_proposal_service.rb
@@ -6,12 +6,12 @@ class NewProposalService
 
   def initialize(proposal_id)
     @proposal = build_proposal(proposal_id)
+    @job_posting = build_job_posting(proposal)
   end
 
   def call
     user = proposal.user
     business = proposal.business
-    job_posting = JobPosting.find_by(public_id: proposal.job_posting_id)
 
     KakaoNotificationService.call(
       template_id: KakaoTemplate::PROPOSAL_RESPONSE_EDIT,
@@ -21,12 +21,12 @@ class NewProposalService
         user_name: user.name,
         business_name: business.name,
         distance: I18n.t("activerecord.attributes.user.preferred_distance.#{user.preferred_distance}"),
-        address: job_posting.address,
-        work_type_ko: translate_type('job_posting', job_posting, :work_type),
-        pay_text: get_pay_text(job_posting),
-        business_vn: job_posting.vn,
-        job_posting_title: job_posting.title,
-        job_posting_public_id: job_posting.public_id,
+        address: @job_posting.address,
+        work_type_ko: translate_type('job_posting', @job_posting, :work_type),
+        pay_text: get_pay_text(@job_posting),
+        business_vn: @job_posting.proposal_vn || @job_posting.vn,
+        job_posting_title: @job_posting.title,
+        job_posting_public_id: @job_posting.public_id,
         target_public_id: user.public_id,
         employee_id: user.public_id,
       },
@@ -37,5 +37,9 @@ class NewProposalService
 
   def build_proposal(proposal_id)
     Proposal.find(proposal_id)
+  end
+
+  def build_job_posting(proposal)
+    JobPosting.find(proposal.job_posting_id)
   end
 end
