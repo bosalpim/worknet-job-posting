d6d38776e091f76a2c8de99183767dc848801cf9
JeongHaMin <wjdgkals74@gmail.com>
2023-10-05 11:40:31 +0900

    FEAT : 관심일자리 요양보호사 메세지 발송 이벤트 로깅 추가

--- app/helpers/kakao_notification_logging_helper.rb
+++ app/helpers/kakao_notification_logging_helper.rb
@@ -83,6 +83,8 @@ module KakaoNotificationLoggingHelper
       return get_call_interview_accepted_logging_data(template_id, tem_params)
     when KakaoTemplate::CALL_SAVED_JOB_CAREGIVER
       return get_call_saved_job_caregiver(template_id, tem_params)
+    when KakaoTemplate::CALL_SAVED_JOB_POSTING_V2
+      return get_call_saved_job_caregiver2(template_id, tem_params)
     when KakaoTemplate::ASK_ACTIVE
       return get_ask_active_logging_data(template_id, tem_params)
     else
@@ -286,6 +288,24 @@ module KakaoNotificationLoggingHelper
         "template" => template_id,
         "centerName" => tem_params[:business_name],
         "jobPostingId" => tem_params[:job_posting_id],
+        "title" => tem_params[:job_posting_title],
+        "type_match" => tem_params[:type_match],
+        "gender_match" => tem_params[:gender_match],
+        "day_match" => tem_params[:day_match],
+        "time_match" => tem_params[:time_match],
+        "grade_match" => tem_params[:grade_match]
+      }
+    }
+  end
+
+  def self.get_call_saved_job_caregiver2(template_id, tem_params)
+    return {
+      "user_id" => tem_params[:target_public_id],
+      "event_type" => NOTIFICATION_EVENT_NAME,
+      "event_properties" => {
+        "template" => template_id,
+        "centerName" => tem_params[:center_name],
+        "jobPostingId" => tem_params[:job_posting_public_id],
         "title" => tem_params[:job_posting_title],
         "type_match" => tem_params[:type_match],
         "gender_match" => tem_params[:gender_match],
