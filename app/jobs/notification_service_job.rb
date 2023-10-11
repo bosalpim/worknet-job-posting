b0541ce360cec96b7bc49027cbd8e50a87b6ebc7
JeongHaMin <wjdgkals74@gmail.com>
2023-10-11 11:01:49 +0900

    FEAT : 메세지 발송 함수 통합, 결과 처리 빈 함수 추가

--- app/jobs/notification_service_job.rb
+++ app/jobs/notification_service_job.rb
@@ -12,11 +12,10 @@ class NotificationServiceJob < ApplicationJob
 
   def process(event)
     # 발송 데이터 생성
-    factory = Notification::FactoryService.create(event[:template_id], event[:params])
+    notification = Notification::FactoryService.create(event[:template_id], event[:params])
     # 발송 (ps. 메세지 성공/실패에 따른 이벤트로깅은 재발송등 사후 처리의 편의성을 위해 Amplitude 로깅이 함께 수행됩니다.)
-    factory.send_app_push # 앱 푸쉬
-    factory.send_bizm_post_pay #
-
+    notification.notify
     # 발송결과 DB 저장 (사후 처리 대상 구분되도록 DB 내역을 생성해야합니다.)
+    notification.save_result
   end
 end
--- app/services/notification/factory/message_factory_class.rb
+++ app/services/notification/factory/message_factory_class.rb
@@ -17,6 +17,18 @@ class Notification::Factory::MessageFactoryClass
   def create_message
     raise NotImplementedError, "#{self.class}에서 해당 '#{__method__}'를 구현하지 않았습니다. Notification::CreateMessage::CallSavedJobPostingV2를 참고하여 개발해주세요."
   end
+
+  def notify
+    send_app_push
+    send_bizm_post_pay
+    send_bizm_pre_pay
+  end
+
+  def save_result
+
+  end
+
+  private
   def send_app_push
     push = @push_list.first
     return if push.nil?
@@ -44,91 +56,15 @@ class Notification::Factory::MessageFactoryClass
   end
 
   def send_bizm_pre_pay
-    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
-  end
-end
-class AppPush
-  include NotificationRequestHelper
-  # FCM
-  def initialize(message_template_id, to, collapse_key, notification, target_public_id)
-    @message_template_id = message_template_id
-    @to = to
-    @collapse_key = collapse_key
-    @notification = notification
-    @target_public_id = target_public_id
-  end
-
-  # FCM
-  def send_request
-    begin
-      response = request_app_push({ to: @to, collapse_key: @collapse_key, notification: @notification })
-      success = response["success"]
-      if success == 1
-        return { status: 'success', response: response, target_public_id: @target_public_id }
-      else
-        return { status: 'fail', response: response.to_s, target_public_id: @target_public_id }
-      end
-    rescue Net::ReadTimeout
-      return { status: 'fail', response: "NET::TIMEOUT", target_public_id: @target_public_id }
-    rescue HTTParty::Error => e
-      return { status: 'fail', response: "#{e.message}", target_public_id: @target_public_id }
+    message = @bizm_pre_pay_list.first
+    return if message.nil?
+    unless message.is_a?(BizmPrePayMessage)
+      raise ArgumentError, "@bizm_post_pay_list에는 BizmPrePayMessage Class만 주입하여 사용합니다."
     end
-  end
-end
 
-class BizmPostPayMessage
-  include NotificationRequestHelper
-  def initialize(message_template_id, message_type = "AT", phone, params, target_public_id)
-    @message_template_id = message_template_id
-    params[:target_public_id] = target_public_id
-    @params = params
-    @target_public_id = target_public_id
-    @bizm_template_service = KakaoTemplateService.new(message_template_id, message_type, phone, nil)
-  end
-
-  def send_request
-    request_params = @bizm_template_service.get_final_request_params(@params, false)
-    begin
-      response = request_post_pay(request_params)
-      response.class == Array ? response.first : response
-      amplitude_log(response)
-      { status: 'success', response: response, target_public_id: @target_public_id }
-    rescue Net::ReadTimeout
-      { status: 'fail', response: "NET::TIMEOUT", target_public_id: @target_public_id }
-    rescue HTTParty::Error => e
-      { status: 'fail', response: "#{e.message}", target_public_id: @target_public_id }
+    @bizm_pre_pay_list.each do |bizm_message|
+      result = bizm_message.send_request
+      @bizm_pre_pay_result.push(result)
     end
-  end
-
-  def amplitude_log(response)
-    KakaoNotificationLoggingHelper.send_log_for_bizmsg(response, @message_template_id, @params)
-  end
-end
-
-class BizmPrePayMessage
-  include NotificationRequestHelper
-  def initialize(message_template_id, phone, params, target_public_id)
-    @message_template_id = message_template_id
-    @params = params
-    @target_public_id = target_public_id
-    @bizm_template_service = KakaoTemplateService.new(message_template_id, message_type, phone, nil)
-  end
-
-  def send_request
-    request_params = @bizm_template_service.get_final_request_params(@params, true)
-    begin
-      response = request_pre_pay(request_params)
-      response.class == Array ? response.first : response
-      amplitude_log(response)
-      { status: 'success', response: response, target_public_id: @target_public_id }
-    rescue Net::ReadTimeout
-      { status: 'fail', response: "NET::TIMEOUT", target_public_id: @target_public_id }
-    rescue HTTParty::Error => e
-      { status: 'fail', response: "#{e.message}", target_public_id: @target_public_id }
-    end
-  end
-
-  def amplitude_log(response)
-    KakaoNotificationLoggingHelper.send_log(response, @message_template_id, @params)
   end
 end
