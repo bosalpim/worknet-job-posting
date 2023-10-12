848622d405f180de43cc141ed90a5e22d0741663
hanwool95 <hanwool95@snu.ac.kr>
2023-10-12 17:13:37 +0900

    FIX: CBT Draft 대상 알림톡 결과 저장 위한 템플릿 지정분 추가 (#246)

--- app/models/kakao_notification_result.rb
+++ app/models/kakao_notification_result.rb
@@ -28,6 +28,7 @@ class KakaoNotificationResult < ApplicationRecord
   NEW_JOB_VISIT_V2 = 'new_job_visit_v2'.freeze
   NEW_JOB_FACILITY_V2 = 'new_job_facility(23-09-2w)'.freeze
   NEWSPAPER_V2 = 'newspaper_v2'.freeze
+  CBT_DRAFT = 'CBT_draft2'.freeze # cbt 가입 draft 1일, 2일, 3일 뒤 대상
 
   # 신규일자리알림
   NEW_JOB_POSTING = 'new_job_posting'.freeze
@@ -68,6 +69,7 @@ class KakaoNotificationResult < ApplicationRecord
     NEW_JOB_VISIT_V2 => NEW_JOB_VISIT_V2,
     NEW_JOB_FACILITY_V2 => NEW_JOB_FACILITY_V2,
     ASK_ACTIVE => ASK_ACTIVE,
-    NEW_JOB_POSTING => NEW_JOB_POSTING
+    NEW_JOB_POSTING => NEW_JOB_POSTING,
+    CBT_DRAFT => CBT_DRAFT
   }
 end
