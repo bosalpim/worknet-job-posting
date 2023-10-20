0ca7d8a942b755fbe707123dac81121ac1b5a766
JeongHaMin <wjdgkals74@gmail.com>
2023-10-20 17:58:49 +0900

    FEAT : 별도 서비스로 메세지 발송대상 추출하기

--- app/services/notification/factory/new_job_notification.rb
+++ app/services/notification/factory/new_job_notification.rb
@@ -1,9 +1,10 @@
 class Notification::Factory::NewJobNotification < Notification::Factory::NotificationFactoryClass
+  NewJobPostingUsersService = Notification::SearchUser::NewJobPostingUsersService
   def initialize(job_posting_id)
     super(MessageTemplateName::NEW_JOB_POSTING)
     job_posting = JobPosting.find(job_posting_id)
     @job_posting = job_posting
-    # @list = 메세지 발송대상 추출 서비스 별도 생성
+    @list = NewJobPostingUsersService.call(job_posting)
     create_message
   end
   def create_message
