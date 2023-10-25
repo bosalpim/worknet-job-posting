55e1d86ff7504fa4fe99fb06f498a98170936cba
JeongHaMin <wjdgkals74@gmail.com>
2023-10-25 11:01:15 +0900

    Update : 메소드 변경

--- app/jobs/new_job_notification_job.rb
+++
@@ -1,7 +0,0 @@
-class NewJobNotificationJob < ApplicationJob
-  def dig
-    job_posting = JobPosting.find(event[:job_posting_id])
-    NewJobNotificationService.test_call(job_posting) unless Jets.env == 'production'
-    NewJobNotificationService.call(job_posting) if Jets.env == 'production'
-  end
-end
