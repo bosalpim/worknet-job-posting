4d528ae70d59e1a17c2dc37b2872922a5ffcbb1c
hanwool95 <hanwool95@snu.ac.kr>
2023-11-09 16:03:48 +0900

    주간 알림톡 점진적 배포 100% 상향 (#292)
    * FEATURE: 누적 draft 대상 점진적 배포
    
    * FEATURE: 누적 자격증 취득 예정자 알림톡 점진적 배포 100%

--- app/services/search_preparative_cbt_users_service.rb
+++ app/services/search_preparative_cbt_users_service.rb
@@ -11,7 +11,7 @@ class SearchPreparativeCbtUsersService
     now = Date.today
     last_month = now << 1
     month_before_last = now << 2
-    ninety_days_ago = now - 31 # Todo 점전적 배포 예정 31 -> 61 -> 91
+    ninety_days_ago = now - 91
     one_days_ago = now - 1
 
     # 연도.월과 연도.월.일 형식으로 변환
@@ -22,33 +22,14 @@ class SearchPreparativeCbtUsersService
     # %y.%m으로 저장된 경우는 이전달, 혹은 이전전달
     # %y.%m.%d로 저장된 경우는 91일전부터 1일전까지
     # 예: 23년 10월 13일 기준 23.08, 23.09, 23.07.14~23.10.12
-    # Todo 점진적 배포. x -> 이전 달 -> 이전전달
 
-    # 1단계 x 달 쿼리
     users = User.where(
-      '(
+      '(expected_acquisition IN (?, ?) AND LENGTH(expected_acquisition) = 7) OR (
         expected_acquisition ~ ? AND expected_acquisition::DATE >= ? AND expected_acquisition::DATE <= ?
         )',
+      month_before_last_str, last_month_str,
       '^\\d{4}/\\d{2}/\\d{1,2}$', ninety_days_ago.strftime('%Y/%m/%d'), one_days_ago.strftime('%Y/%m/%d')
     )
-
-    # 2단계 이전 달 쿼리
-    # users = User.where(
-    #   '(expected_acquisition IN (?) AND LENGTH(expected_acquisition) = 5) OR (
-    #         expected_acquisition ~ ? AND expected_acquisition::DATE >= ? AND expected_acquisition::DATE <= ?
-    #         )',
-    #   last_month_str,
-    #   '^\\d{4}/\\d{2}/\\d{1,2}$', ninety_days_ago.strftime('%Y/%m/%d'), one_days_ago.strftime('%Y/%m/%d')
-    # )
-
-    # 3단계 이전전달 쿼리
-    # users = User.where(
-    #   '(expected_acquisition IN (?, ?) AND LENGTH(expected_acquisition) = 5) OR (
-    #     #         expected_acquisition ~ ? AND expected_acquisition::DATE >= ? AND expected_acquisition::DATE <= ?
-    #     #         )',
-    #   month_before_last_str, last_month_str,
-    #   '^\\d{4}/\\d{2}/\\d{1,2}$', ninety_days_ago.strftime('%Y/%m/%d'), one_days_ago.strftime('%Y/%m/%d')
-    # )
     users.where(has_certification: false, status: 'active')
 
   end
