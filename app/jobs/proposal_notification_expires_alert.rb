fd276b814927e7a5ff2c23f52dcdf1788f539c00
80xer <80xer.s@gmail.com>
2023-11-14 11:34:43 +0900

    wip: 전화면접제안 알림 만료 경고 (#315)
    * wip: 전화면접제안 알림 만료 경고
    
    * wip: 전화면접 제안 종료 알림톡
    
    * wip: 전화면접 제안 종료 알림톡
    
    * wip: 전화면접 제안 종료 알림톡
    
    * feat: remove comments
    
    * feat: remove comments
    
    * fix: file name
    
    * fix: 파일삭제

--- app/jobs/proposal_notification_expires_alert.rb
+++
@@ -1,8 +0,0 @@
-class ProposalNotificationExpiresAlert < ApplicationJob
-  # (한국 시간) 매일 오전 9시에 전화면접 제안 알림 켜진 유저중 만료일이 1일 이하 남은 유저에게 발송
-  cron "0 0 * * ? *"
-
-  def alert_expires_proposal_notification
-    ProposalNotificationExpiresService.call
-  end
-end
