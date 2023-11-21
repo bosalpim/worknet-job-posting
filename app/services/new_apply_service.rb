5991dc32e4a79af7a418c1e7e1772f4d610ce76a
jhpung <jhpung.dev@gmail.com>
2023-11-21 11:46:04 +0900

    cut: 전화요청 send_text_message 제거

--- app/services/new_apply_service.rb
+++ app/services/new_apply_service.rb
@@ -35,12 +35,6 @@ class NewApplyService
         short_url: short_url
       }
     )
-    send_text_message(
-      phone_number: job_posting.manager_phone_number,
-      business_name: business.name,
-      job_posting_title: job_posting.title,
-      short_url: build_mobile_url(apply)
-    )
     save_kakao_notification(
       response,
       NotificationResult::CALL_REQUEST_ALARM,
@@ -68,39 +62,6 @@ class NewApplyService
       base_url
     )
     short_url.url
-  end
-
-  def send_text_message(
-    phone_number:,
-    business_name:,
-    job_posting_title:,
-    short_url:
-  )
-    if Lms.new(
-      phone_number: phone_number,
-      message: "[케어파트너] 전화요청 알림
-
-#{business_name} 담당자님
-등록하신 공고에 전화를 요청한 요양보호사가 있습니다.
-
-아래 버튼 혹은 링크를 눌러 요양보호사 정보를 확인하고 전화해보세요.
-빠르게 연락할수록 채용확률이 높아집니다.
-
-공고명: #{job_posting_title}
-링크: #{short_url}"
-    ).send
-      AmplitudeService.instance.log_array([{
-                                             "user_id" => @client.public_id,
-                                             "event_type" => KakaoNotificationLoggingHelper::NOTIFICATION_EVENT_NAME,
-                                             "event_properties" => {
-                                               type: 'text_message',
-                                               template: MessageTemplateName::CALL_REQUEST_ALARM,
-                                               jobPostingId: @job_posting.public_id,
-                                               title: @job_posting.title,
-                                               employee_id: @user.public_id
-                                             }
-                                           }])
-    end
   end
 
   def build_short_url(apply)
