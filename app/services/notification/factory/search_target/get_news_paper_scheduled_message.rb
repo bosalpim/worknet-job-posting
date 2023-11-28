df6bfac8763a826248804d4b06ac5b9e2acdd269
JeongHaMin <wjdgkals74@gmail.com>
2023-11-28 09:55:49 +0900

    Update get_news_paper_scheduled_message.rb

--- app/services/notification/factory/search_target/get_news_paper_scheduled_message.rb
+++ app/services/notification/factory/search_target/get_news_paper_scheduled_message.rb
@@ -7,7 +7,6 @@ class Notification::Factory::SearchTarget::GetNewsPaperScheduledMessage
     message_created_time = 1
 
     total_count = ScheduledMessageCount.where(created_at: message_created_time.days.ago..).where(template_id: template_id).first!.total_count
-    p total_count
     counts = calculate_sent_and_message_count(total_count, should_send_percent, sent_percent)
     message_count = counts.dig(:message_count)
     sent_count = counts.dig(:sent_count)
