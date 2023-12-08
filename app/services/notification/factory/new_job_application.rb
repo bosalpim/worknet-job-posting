b5f64c99dbb241e67ae8881cdd939c21d97d67bf
jhpung <jhpung.dev@gmail.com>
2023-12-09 03:35:42 +0900

    fix: 간편 지원 알림톡 수정

--- app/services/notification/factory/new_job_application.rb
+++ app/services/notification/factory/new_job_application.rb
@@ -30,16 +30,16 @@ class Notification::Factory::NewJobApplication < Notification::Factory::Notifica
   end
 
   def create_bizm_message
-    user_info = [user.name, user.korean_gender, user.birth_year.present? ? "#{calculate_korean_age(user.birth_year)}세" : nil]
+    user_info = [@user.name, @user.korean_gender, @user.birth_year.present? ? "#{calculate_korean_age(@user.birth_year)}세" : nil]
                   .filter { |i| i.present? }
                   .join('/')
-    arlimtalk_utm = "utm_source=message&utm_medium=arlimtalk&utm_campaign=#{MessageTemplateName::JOB_APPLICATION}"
+    arlimtalk_utm = "utm_source=message&utm_medium=arlimtalk&utm_campaign=#{@message_template_id}"
     suffix = "/employment_management/job_applications/#{@job_application.public_id}"
 
     link = if Jets.env.production?
              "https://business.carepartner.kr#{suffix}?#{arlimtalk_utm}"
            elsif Jets.env.staging?
-             "https://staging-business.vercel.app#{suffix}?#{arlimtalk_utm}"
+             "https://business.dev-carepartner.kr#{suffix}?#{arlimtalk_utm}"
            else
              "https://staging-business.vercel.app#{suffix}?#{arlimtalk_utm}"
            end
@@ -53,11 +53,11 @@ class Notification::Factory::NewJobApplication < Notification::Factory::Notifica
       user_info: user_info,
       user_message: @job_application.user_message,
       preferred_call_time: @job_application.preferred_call_time,
-      type_match: is_type_match(user.preferred_work_types, job_posting.work_type),
-      gender_match: is_gender_match(user.preferred_gender, job_posting.gender),
-      day_match: is_day_match(user.job_search_days, job_posting.working_days),
-      time_match: is_time_match(work_start_time: job_posting.work_start_time, work_end_time: job_posting.work_end_time, job_search_times: user.job_search_times),
-      grade_match: is_grade_match(user.preferred_grades, job_posting.grade),
+      type_match: is_type_match(@user.preferred_work_types, @job_posting.work_type),
+      gender_match: is_gender_match(@user.preferred_gender, @job_posting.gender),
+      day_match: is_day_match(@user.job_search_days, @job_posting.working_days),
+      time_match: is_time_match(work_start_time: @job_posting.work_start_time, work_end_time: @job_posting.work_end_time, job_search_times: @user.job_search_times),
+      grade_match: is_grade_match(@user.preferred_grades, @job_posting.grade),
       link: link,
     }
     @bizm_post_pay_list.push(BizmPostPayMessage.new(
