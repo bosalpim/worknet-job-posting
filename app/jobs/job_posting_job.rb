95c82cd17e76c7da7e4d671f727a4b7e048f0693
JeongHaMin <wjdgkals74@gmail.com>
2023-12-11 18:35:22 +0900

    Update job_posting_job.rb

--- app/jobs/job_posting_job.rb
+++ app/jobs/job_posting_job.rb
@@ -91,9 +91,9 @@ class JobPostingJob < ApplicationJob
         # 1차 메세지 발송 완료 히스토리 & 2차 예약 히스토리 생성
         MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 1, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id)
         # 1차가 내일 오전8시로 예약된다면, 2차 발송 예약 시간은 2일뒤가 되어야한다.
-        scheduled_at = Time.current
+        scheduled_at = Time.current.tomorrow.beginning_of_day + 8.hours - 9.hours
         second_history = MessageHistory.create!(type_name: TYPE_RESERVED, status: 2, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
-        second_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 2, scheduled_at_text: (scheduled_at).strftime('%m월 %d일 %I시 %M분') })
+        second_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 2, scheduled_at_text: (scheduled_at + 9.hours).strftime('%m월 %d일 %I시 %M분') })
         second_reserve_notification.process
 
         second_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_SECOND, { job_posting_id: job_posting_id })
@@ -103,7 +103,7 @@ class JobPostingJob < ApplicationJob
         MessageHistory.create!(type_name: TYPE_RESERVED, status: 3, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
         second_history.update!(is_cancel: true)
 
-        third_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 3, scheduled_at_text: (scheduled_at).strftime('%m월 %d일 %I시 %M분') })
+        third_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 3, scheduled_at_text: (scheduled_at + 9.hours).strftime('%m월 %d일 %I시 %M분') })
         third_reserve_notification.process
 
         third_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_THIRD, { job_posting_id: job_posting_id })
