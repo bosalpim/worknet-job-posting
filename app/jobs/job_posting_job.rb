b919e4593e1ed5ef2e0a809ce9a249bed95acbcc
JeongHaMin <wjdgkals74@gmail.com>
2023-12-12 12:55:55 +0900

    Update job_posting_job.rb

--- app/jobs/job_posting_job.rb
+++ app/jobs/job_posting_job.rb
@@ -85,44 +85,45 @@ class JobPostingJob < ApplicationJob
   end
   def first_message
     job_posting_id = event["job_posting_id"]
-    if Jets.env != 'production'
-      begin
-        notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_FIRST, { job_posting_id: job_posting_id })
-        notification.process
-
-        # 1차 메세지 발송 완료 히스토리 & 2차 예약 히스토리 생성
-        MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 1, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id)
-        # 1차가 내일 오전8시로 예약된다면, 2차 발송 예약 시간은 2일뒤가 되어야한다.
-        scheduled_at = Time.current.tomorrow.beginning_of_day + RESERVE_TARGET_TIME - KOREAN_OFFSET
-        second_history = MessageHistory.create!(type_name: TYPE_RESERVED, status: 2, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
-        second_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 2, scheduled_at_text: (scheduled_at + KOREAN_OFFSET).strftime('%m월 %d일 %I시 %M분') })
-        second_reserve_notification.process
-
-        second_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_SECOND, { job_posting_id: job_posting_id })
-        second_notification.process
-
-        MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 2, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
-        MessageHistory.create!(type_name: TYPE_RESERVED, status: 3, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
-        second_history.update!(is_cancel: true)
-
-        third_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 3, scheduled_at_text: (scheduled_at + KOREAN_OFFSET).strftime('%m월 %d일 %I시 %M분') })
-        third_reserve_notification.process
-
-        third_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_THIRD, { job_posting_id: job_posting_id })
-        third_notification.process
-
-        MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 3, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
-        MessageHistory.create!(type_name: TYPE_ENDED, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
-
-        ended_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_ENDED, { job_posting_id: job_posting_id })
-        ended_notification.process
-
-        return
-      rescue => e
-        Jets.logger.info e.message
-      end
-      return
-    end
+    # Staging 테스트 코드
+    # if Jets.env != 'production'
+    #   begin
+    #     notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_FIRST, { job_posting_id: job_posting_id })
+    #     notification.process
+    #
+    #     # 1차 메세지 발송 완료 히스토리 & 2차 예약 히스토리 생성
+    #     MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 1, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id)
+    #     # 1차가 내일 오전8시로 예약된다면, 2차 발송 예약 시간은 2일뒤가 되어야한다.
+    #     scheduled_at = Time.current.tomorrow.beginning_of_day + RESERVE_TARGET_TIME - KOREAN_OFFSET
+    #     second_history = MessageHistory.create!(type_name: TYPE_RESERVED, status: 2, notification_relate_instance_types_id: 1, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
+    #     second_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 2, scheduled_at_text: (scheduled_at + KOREAN_OFFSET).strftime('%m월 %d일 %I시 %M분') })
+    #     second_reserve_notification.process
+    #
+    #     second_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_SECOND, { job_posting_id: job_posting_id })
+    #     second_notification.process
+    #
+    #     MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 2, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
+    #     MessageHistory.create!(type_name: TYPE_RESERVED, status: 3, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id, scheduled_at: scheduled_at)
+    #     second_history.update!(is_cancel: true)
+    #
+    #     third_reserve_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_RESERVE, { job_posting_id: job_posting_id, times: 3, scheduled_at_text: (scheduled_at + KOREAN_OFFSET).strftime('%m월 %d일 %I시 %M분') })
+    #     third_reserve_notification.process
+    #
+    #     third_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_MESSAGE_THIRD, { job_posting_id: job_posting_id })
+    #     third_notification.process
+    #
+    #     MessageHistory.create!(type_name: TYPE_SUCCEDED, status: 3, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
+    #     MessageHistory.create!(type_name: TYPE_ENDED, notification_relate_instance_types_id: RELATE_TYPE_JOB_POSTING, notification_relate_instance_id: job_posting_id)
+    #
+    #     ended_notification = Notification::FactoryService.create(MessageTemplateName::JOB_ADS_ENDED, { job_posting_id: job_posting_id })
+    #     ended_notification.process
+    #
+    #     return
+    #   rescue => e
+    #     Jets.logger.info e.message
+    #   end
+    #   return
+    # end
 
     begin
       american_time = Time.current
