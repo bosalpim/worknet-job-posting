898100cbd9604f09808958917dabb17c4e70a0fe
delee <de.lee@bosalpim.co.kr>
2023-12-20 17:11:47 +0900

    feat: 채용인증 요청 템플릿 수정

--- app/services/kakao_template_service.rb
+++ app/services/kakao_template_service.rb
@@ -153,6 +153,8 @@ class KakaoTemplateService
       get_job_ads_message_reserve(tem_params)
     when MessageTemplateName::JOB_ADS_ENDED
       get_job_ads_ended(tem_params)
+    when MessageTemplateName::CONFIRM_CAREER_CERTIFICATION
+      get_confirm_career_certification_message(tem_params)
     else
       Jets.logger.info "존재하지 않는 메시지 템플릿 요청입니다: template_id: #{template_id}, tem_params: #{tem_params.to_json}"
     end
@@ -2045,6 +2047,34 @@ carepartner.kr#{path}
           name: "문의 그만받기 (채용종료)",
           url_mobile: close_link,
           url_pc: close_link,
+        }
+      ]
+    }
+  end
+
+  def get_confirm_career_certification_message(tem_params)
+    job_posting_title = tem_params[:job_posting_title]
+    user_info = tem_params[:user_info]
+    user_name = tem_params[:user_name]
+    link = tem_params[:link]
+    {
+      title: "#{user_name} 요양보호사가 아래 공고에 채용인증을 요청했어요. 맞다면 채용 결과 입력 후 전화번호 열람권(3장)을 받아 보세요.",
+      message: "#{user_name} 요양보호사가 아래 공고에 채용인증을 요청했어요. 맞다면 채용 결과 입력 후 전화번호 열람권(3장)을 받아 보세요.
+
+■ 공고
+#{job_posting_title}
+
+■ 요양보호사
+#{user_info}
+
+■ 혜택안내
+채용 결과를 입력하면 요양보호사에게 즉시 전화할 수 있는 전화번호 열람권(3장/ 4,500원 상당)을 드려요.",
+      buttons: [
+        {
+          type: "WL",
+          name: "채용종료하기",
+          url_mobile: link,
+          url_pc: link,
         }
       ]
     }
