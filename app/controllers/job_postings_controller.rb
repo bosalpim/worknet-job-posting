774863736bfc8e8e00601ee072187e60a2022b1a
JeongHaMin <wjdgkals74@gmail.com>
2023-12-20 18:53:41 +0900

    FEAT : 공고등록 완료 알림 추가

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -11,8 +11,14 @@ class JobPostingsController < ApplicationController
 
   def new_notification
     event = { job_posting_id: params["job_posting_id"] }
+    # 요보사:신규 일자리 알림
     NotificationServiceJob.perform_now(:notify, { message_template_id: MessageTemplateName::NEW_JOB_POSTING, params: { job_posting_id: event[:job_posting_id] } }) if Jets.env.development?
     NotificationServiceJob.perform_later(:notify, { message_template_id: MessageTemplateName::NEW_JOB_POSTING, params: { job_posting_id: event[:job_posting_id] } }) unless Jets.env.development?
+
+    # 기관:공고 등록 알림
+    NotificationServiceJob.perform_now(:notify, { message_template_id: MessageTemplateName::BUSINESS_JOB_POSTING_COMPLETE, params: { job_posting_id: event[:job_posting_id] } }) if Jets.env.development?
+    NotificationServiceJob.perform_later(:notify, { message_template_id: MessageTemplateName::BUSINESS_JOB_POSTING_COMPLETE, params: { job_posting_id: event[:job_posting_id] } }) unless Jets.env.development?
+
     render json: {
       success: true
     }, status: :ok
