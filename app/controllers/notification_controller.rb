40363202485471a33154809eb8ce371dae4797df
JeongHaMin <wjdgkals74@gmail.com>
2023-12-21 13:24:22 +0900

    FEAT : zion, bbas 메세지 발송 요청에 대한 기본 처리 endpoint 추가, 공고 등록 완료 안내 메세지 발송 추가 (#403)
    * FEAT : zion, bbas 메세지 발송 요청에 대한 기본 처리 endpoint 추가
    
    * FEAT : 공고 등록 완료 안내 대응
    
    * Update routes.rb

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 class NotificationController < ApplicationController
+  def send_message
+    begin
+      case params[:template]
+      when MessageTemplateName::BUSINESS_JOB_POSTING_COMPLETE
+        NotificationServiceJob.perform_now(:notify, { message_template_id: MessageTemplateName::BUSINESS_JOB_POSTING_COMPLETE, params: { job_posting_id: params[:job_posting_id] } }) if Jets.env.development?
+        NotificationServiceJob.perform_later(:notify, { message_template_id: MessageTemplateName::BUSINESS_JOB_POSTING_COMPLETE, params: { job_posting_id: params[:job_posting_id] } }) unless Jets.env.development?
+      else
+        Jets.logger.info "#{params} 요청 대응 case 추가 필요"
+      end
+
+      render json: {
+        success: true
+      }, status: :ok
+    rescue => e
+      Jets.logger.info "#{params}, Error: #{e.message}"
+      render json: {
+        success: true
+      }, status: :ok
+    end
+  end
   def ask_active
     Notification::AskActiveService.new(ask_active_params).call
   end
