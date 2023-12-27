f2ea27b51b487cb6b3cbfdccb3608ee4defa60c5
jhpung <jhpung.dev@gmail.com>
2023-12-27 10:35:45 +0900

    fix: end

--- app/services/notification/factory/search_target/new_job_posting_users_service.rb
+++ app/services/notification/factory/search_target/new_job_posting_users_service.rb
@@ -21,34 +21,33 @@ class Notification::Factory::SearchTarget::NewJobPostingUsersService
     end
 
     unless @job_posting.lat.present? && @job_posting.lng.present?
-      return
+      return users
     end
 
     prefer_work_type = @job_posting.work_type == 'hospital' ? 'etc' : @job_posting.work_type
 
     User.preferred_distances.each do |key, value|
-        users += User
-                   .receive_job_notifications
-                   .select(
-                     "users.*, earth_distance(ll_to_earth(lat, lng), ll_to_earth(#{@job_posting.lat}, #{@job_posting.lng})) AS distance",
-                   )
-                   .within_radius(
-                     DISTANCE_LIST[key.to_sym],
-                     @job_posting.lat,
-                     @job_posting.lng
-                   )
-                   .where(preferred_distance: key)
-                   .where(
-                     'preferred_work_types::jsonb ? :type',
-                     type: prefer_work_type,
-                   )
-                   .where('id not in (?)', users.empty? ? [0] : users.map(&:id))
-                   .where(
-                     'has_certification = true OR expected_acquisition in (?)',
-                     %w[2022/05 2022/08 2022/11 2023/02],
-                   )
-                   .active
-      end
+      users += User
+                 .receive_job_notifications
+                 .select(
+                   "users.*, earth_distance(ll_to_earth(lat, lng), ll_to_earth(#{@job_posting.lat}, #{@job_posting.lng})) AS distance",
+                 )
+                 .within_radius(
+                   DISTANCE_LIST[key.to_sym],
+                   @job_posting.lat,
+                   @job_posting.lng
+                 )
+                 .where(preferred_distance: key)
+                 .where(
+                   'preferred_work_types::jsonb ? :type',
+                   type: prefer_work_type,
+                 )
+                 .where('id not in (?)', users.empty? ? [0] : users.map(&:id))
+                 .where(
+                   'has_certification = true OR expected_acquisition in (?)',
+                   %w[2022/05 2022/08 2022/11 2023/02],
+                 )
+                 .active
     end
 
     users
