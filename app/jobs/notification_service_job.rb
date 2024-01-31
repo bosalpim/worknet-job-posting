860181475828f57e3e80837070cc70872aa50d81
JeongHaMin <wjdgkals74@gmail.com>
2024-01-31 17:32:10 +0900

    FIX : Create Notification (#442)

--- app/jobs/notification_service_job.rb
+++ app/jobs/notification_service_job.rb
@@ -34,12 +34,9 @@ class NotificationServiceJob < ApplicationJob
     begin
       # 발송 데이터 생성
       notification = Notification::FactoryService.create(event[:message_template_id], event[:params])
-      # 발송 (ps. 메세지 성공/실패에 따른 이벤트로깅은 재발송등 사후 처리의 편의성을 위해 Amplitude 로깅이 함께 수행됩니다.)
-      notification.notify
-      # 발송결과 DB 저장 (사후 처리 대상 구분되도록 DB 내역을 생성해야합니다.)
-      notification.save_result
+      notification.process
     rescue => e
-      console.error(e)
+      Jets.logger.info e.message
     end
   end
 end
