df74a0d19b64af00cbac124be3ba78a9fc62fc65
jhpung <jhpung.dev@gmail.com>
2024-02-06 15:39:25 +0900

    Update notification_service_job.rb

--- app/jobs/notification_service_job.rb
+++ app/jobs/notification_service_job.rb
@@ -4,28 +4,33 @@ class NotificationServiceJob < ApplicationJob
   end
 
   cron "0 4 ? * * *"
+
   def notify_saved_job_user_1day_ago
     process({ message_template_id: MessageTemplateName::CALL_SAVED_JOB_POSTING_V2 })
   end
 
   cron "0 7 ? * * *"
+
   def cbt_draft_until_3day
     process({ message_template_id: MessageTemplateName::CBT_DRAFT })
   end
 
   cron "0 7 ? * * *"
+
   def notify_draft_new_user
     process({ message_template_id: MessageTemplateName::CAREPARTNER_PRESENT })
   end
 
   cron "0 7 ? * 6 *"
+
   def notify_3month_draft_user
-    process({ message_template_id: MessageTemplateName::ACCUMULATED_DRAFT})
+    process({ message_template_id: MessageTemplateName::ACCUMULATED_DRAFT })
   end
 
   cron "0 7 ? * 6 *"
+
   def cbt_expected_acquisition_user
-    process({ message_template_id: MessageTemplateName::ACCUMULATED_PREPARATIVE})
+    process({ message_template_id: MessageTemplateName::ACCUMULATED_PREPARATIVE })
   end
 
   private
@@ -36,7 +41,7 @@ class NotificationServiceJob < ApplicationJob
       notification = Notification::FactoryService.create(event[:message_template_id], event[:params])
       notification.process
     rescue => e
-      Jets.logger.info e.message
+      Jets.logger.info e.full_message
     end
   end
 end
