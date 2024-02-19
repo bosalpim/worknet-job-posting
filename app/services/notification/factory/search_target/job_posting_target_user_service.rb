6fe27145c06116309e113ca62c109e31c3883627
hanwool95 <hanwool95@snu.ac.kr>
2024-02-19 11:11:45 +0900

    FIX: 필요 없는 조건인 gender와 distacne 제거 (#452)

--- app/services/notification/factory/search_target/job_posting_target_user_service.rb
+++ app/services/notification/factory/search_target/job_posting_target_user_service.rb
@@ -1,20 +1,12 @@
 class Notification::Factory::SearchTarget::JobPostingTargetUserService
-  NEW_JOB_POSTING_TARGET_DISTANCE_MAP = {
-    by_walk15: 1800,
-    by_walk30: 3000,
-    by_km_3: 5000,
-    by_km_5: 5000,
-  }
 
-  def self.call(lat, lng, distance, gender)
-    new(lat, lng, distance, gender).call
+  def self.call(lat, lng)
+    new(lat, lng).call
   end
 
-  def initialize(lat, lng, distance, gender)
+  def initialize(lat, lng)
     @lat = lat
     @lng = lng
-    @distance = distance
-    @gender = gender
   end
 
   def call
@@ -22,12 +14,10 @@ class Notification::Factory::SearchTarget::JobPostingTargetUserService
     users = User
         .receive_job_notifications
         .within_radius(
-         NEW_JOB_POSTING_TARGET_DISTANCE_MAP[@distance],
+          3000,
          @lat,
          @lng
        )
-       .where(gender: @gender, has_certification: true)
-       .active
 
     if Jets.env != 'production'
       return [User.last]
