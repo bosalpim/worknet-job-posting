b537d1ca10fc212f3fee217e89d3f155b870843e
hanwool95 <hanwool95@snu.ac.kr>
2024-02-20 15:58:08 +0900

    FEATURE: 수신 대상자 몇명인지 확보하는 쿼리 제작 완료

--- app/services/notification/factory/target_job_posting_performance.rb
+++ app/services/notification/factory/target_job_posting_performance.rb
@@ -10,12 +10,21 @@ class Notification::Factory::TargetJobPostingPerformance < Notification::Factory
       Jets.logger.info "-------------- INFO START --------------\n"
       Jets.logger.info "케어파트너 대상 공고 : #{job_posting.public_id}\n"
 
-
-      params = {
-        target_public_id: job_posting.public_id,
-        title: job_posting.title
-      }
-      @bizm_post_pay_list.push(BizmPostPayMessage.new(@message_template_id, job_posting.manager_phone_number, params, job_posting.public_id, "AI"))
+      count = DispatchedNotification.where(notification_relate_instance_types_id: 3,
+                                           notification_relate_instance_id: job_posting.id)
+                                    .where.not(confirmed: nil)
+                                    .count
+      if count > 0
+        Jets.logger.info "수신 대상자 : #{count}명\n"
+        params = {
+          target_public_id: job_posting.public_id,
+          title: job_posting.title,
+          count: count
+        }
+        @bizm_post_pay_list.push(BizmPostPayMessage.new(@message_template_id, job_posting.manager_phone_number, params, job_posting.public_id, "AI"))
+      else
+        Jets.logger.info "수신 대상자 없음 종료\n"
+      end
       Jets.logger.info "-------------- INFO END --------------\n"
     end
   end
