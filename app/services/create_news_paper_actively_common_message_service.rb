747f0f39fbefbc233ff6afe419d9815065a2dcf0
jhpung <jhpung.dev@gmail.com>
2024-02-21 22:21:58 +0900

    feat: 일자리 신문 생성 잡 수정

--- app/services/create_news_paper_actively_common_message_service.rb
+++ app/services/create_news_paper_actively_common_message_service.rb
@@ -1,5 +1,6 @@
-class CreateNewsPaperActivelyCommonMessageService < CreateScheduledMessageService
+class CreateNewsPaperActivelyCommonMessageService < BulkWriteScheduledMessageService
   include NewsPaper
+
   def initialize
     super(
       # 적극구직, 구직중 모두 적극구직 중 유저가 받는 신문으로 통일합니다.
