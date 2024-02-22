7c4bf2abe605c0b3c16de9a3ca46e473b3eb0478
hanwool95 <hanwool95@snu.ac.kr>
2024-02-22 22:55:22 +0900

    FEATURE: 타겟 메세지 로깅 추가(신규 알림톡과 동일)

--- app/helpers/kakao_notification_logging_helper.rb
+++ app/helpers/kakao_notification_logging_helper.rb
@@ -716,6 +716,12 @@ module KakaoNotificationLoggingHelper
       "event_type" => NOTIFICATION_EVENT_NAME,
       "event_properties" => {
         "template" => template_id,
+        "job_posting_id" => tem_params[:job_posting_id],
+        "job_posting_public_id" => tem_params[:job_posting_public_id],
+        "job_posting_title" => tem_params[:job_posting_title],
+        "business_name" => tem_params[:business_name],
+        "job_posting_type" => tem_params[:job_posting_type],
+        "send_at" => tem_params[:send_at],
         "title" => "Target Message",
       }
     }
