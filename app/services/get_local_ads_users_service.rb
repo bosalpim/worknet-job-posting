2c3735317994fe7110039c32232d193ff7ce3916
hanwool95 <hanwool95@snu.ac.kr>
2024-03-11 14:44:51 +0900

    FEATURE: 성과 공유 알림톡에 target-notification-2도 보내도록 조건문 OR 추가

--- app/services/get_local_ads_users_service.rb
+++ app/services/get_local_ads_users_service.rb
@@ -5,7 +5,7 @@ class GetLocalAdsUsersService
 
   def call
     JobPosting.joins(:paid_job_posting_features)
-              .where(paid_job_posting_features: { feature: 'target-notification' })
+              .where(paid_job_posting_features: { feature: %w[target-notification target-notification-2] })
               .not_closed
   end
 end
