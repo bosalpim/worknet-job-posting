77db7575fc8ba929996e31b0cb9d8c121725a0db
jhpung <jhpung.dev@gmail.com>
2024-03-13 11:55:05 +0900

    feat: 동네광고 알림톡 발송 API (#509)
    * feat: 동네광고 알림톡 발송 API
    
    * feat: 공고유형에 따라 거리조건 분기

--- app/modules/message_template_name.rb
+++ app/modules/message_template_name.rb
@@ -52,8 +52,8 @@ module MessageTemplateName
   TARGET_JOB_POSTING_AD = 'target_job_posting_ad'.freeze # 동네 광고 유도
   TARGET_JOB_POSTING_PERFORMANCE = 'target_job_posting_performance'.freeze # 동네광고 성과
 
-  #B2G 관련
-  JOB_SUPPORT_REQUEST_AGREEMENT= 'job_support_request_agreement'.freeze # 일자리 지원사업 요보사 동의 요청
+  # B2G 관련
+  JOB_SUPPORT_REQUEST_AGREEMENT = 'job_support_request_agreement'.freeze # 일자리 지원사업 요보사 동의 요청
 
   CBT_DRAFT = 'CBT_draft2'.freeze # cbt 가입 draft 1일, 2일, 3일 뒤 대상
   CAREPARTNER_PRESENT = 'carepartner_present'.freeze
@@ -105,4 +105,7 @@ module MessageTemplateName
 
   # 돌봄플러스 신청 알림
   NONE_LTC_REQUEST = 'nonltc_01'.freeze
+
+  # 동네광고 신규일자리 알림 V2
+  TARGET_USER_JOB_POSTING_V2 = 'target_user_job_posting_V2'.freeze
 end
--- app/services/kakao_template_service.rb
+++ app/services/kakao_template_service.rb
@@ -49,6 +49,8 @@ class KakaoTemplateService
 
   def get_template_data(tem_params)
     case @template_id
+    when MessageTemplateName::TARGET_USER_JOB_POSTING_V2
+      get_target_user_job_posting_v2_data(tem_params)
     when MessageTemplateName::PROPOSAL
       get_proposal_data(tem_params)
     when MessageTemplateName::NEW_JOB_POSTING_VISIT
@@ -259,6 +261,34 @@ class KakaoTemplateService
     end
 
     data
+  end
+
+  def get_target_user_job_posting_v2_data(tem_params)
+    view_link = tem_params[:view_link]
+    application_link = tem_params[:application_link]
+    contact_link = tem_params[:contact_link]
+
+    {
+      title: tem_params[:title],
+      message: tem_params[:message],
+      buttons: [
+        {
+          name: '⚡️ 간편 지원하기',
+          type: 'WL',
+          url_mobile: application_link
+        },
+        {
+          name: '💬️ 문자 문의하기',
+          type: 'WL',
+          url_mobile: contact_link
+        },
+        {
+          name: '🔎 일자리 확인하기',
+          type: 'WL',
+          url_mobile: view_link
+        }
+      ]
+    }
   end
 
   def get_business_job_posting_complete(tem_params)
