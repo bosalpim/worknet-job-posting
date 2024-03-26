7edac740bed2a27d49632c4a1b910e3d7610eb0b
jhpung <jhpung.dev@gmail.com>
2024-03-26 17:48:24 +0900

    feat: 일자리신문 구조 수정

--- app/services/notification/factory/send_news_paper_v2.rb
+++ app/services/notification/factory/send_news_paper_v2.rb
@@ -1,32 +1,36 @@
 class Notification::Factory::SendNewsPaperV2 < Notification::Factory::NotificationFactoryClass
   include KakaoNotificationLoggingHelper
 
-  def initialize(newspapers = Newspaper.where(id: nil))
+  def initialize(newspapers = Newspaper.where(id: nil).limit(10))
     super(MessageTemplateName::NEWSPAPER_V2)
+
+    @list = newspapers
 
     create_message
   end
 
   def create_message
-    @list.each do |message|
-      template_params = JSON.parse(message.content)
-      message_target_medium = template_params["target_medium"]
-      send_app_push = @target_medium == APP_PUSH && message_target_medium == APP_PUSH
-      if send_app_push
-        push_token = template_params["push_token"]
-        path = "/newspaper?lat=#{template_params["lat"]}&lng=#{template_params["lng"]}&utm_source=message&utm_medium=#{NOTIFICATION_TYPE_APP_PUSH}&utm_campaign=newspaper_job_alarm"
-        date = NewsPaper::get_today_date
+    @list.each do |newspaper|
+      user = newspaper.user
+
+      if user&.lat.nil? || user&.lng.nil?
+        next
+      end
+
+      base_path = "/newspaper?lat=#{user.lat}&lng=#{user.lng}"
+      if user.is_sendable_app_push?
+        link = "#{base_path}&utm_source=message&utm_medium=#{NOTIFICATION_TYPE_APP_PUSH}&utm_campaign=newspaper_job_alarm"
         @app_push_list.push(
           AppPush.new(
             @message_template_id,
             push_token,
             nil,
             {
-              title: "#{date} 우리동네 요양일자리 신문이 도착했어요!",
+              title: "우리동네 요양일자리 신문이 도착했어요!",
               body: "지금 바로 맞춤 일자리를 확인해보세요.",
-              link: "#{Main::Application::DEEP_LINK_SCHEME}#{path}"
+              link: "#{Main::Application::DEEP_LINK_SCHEME}#{link}"
             },
-            template_params["target_public_id"],
+            user.public_id,
             {
               "sender_type" => SENDER_TYPE_CAREPARTNER,
               "receiver_type" => RECEIVER_TYPE_USER,
@@ -35,8 +39,21 @@ class Notification::Factory::SendNewsPaperV2 < Notification::Factory::Notificati
             }
           )
         )
-      else
-        @bizm_post_pay_list.push(BizmPostPayMessage.new(@message_template_id, message.phone_number, template_params, template_params["target_public_id"], "AI", nil, [0]))
+      elsif user.phone_number.present?
+        link = "#{base_path}&utm_source=message&utm_medium=#{NOTIFICATION_TYPE_KAKAO}&utm_campaign=newspaper_job_alarm"
+        @bizm_post_pay_list.push(
+          BizmPostPayMessage.new(
+            @message_template_id,
+            user.phone_number,
+            {
+              link: link
+            },
+            user.public_id,
+            "AI",
+            nil,
+            [0]
+          )
+        )
       end
     end
   end
