bb0e54431f7b87bfbc3d53acfaec3481d25a7fc5
jhpung <jhpung.dev@gmail.com>
2024-03-28 07:58:00 +0900

    일자리신문 (#535)
    * wip: 일자리신문 발송 구조
    
    * Update send_newspaper_job.rb
    
    * Update send_newspaper_job.rb
    
    * wip: newspaper
    
    * wip: news test
    
    * wip: fix
    
    * fix: newspaper
    
    * wip: fix queue
    
    * cut
    
    * fix: sqs_event_paylaod
    
    * fix
    
    * Update send_newspaper_job.rb
    
    * wip: 일자리신문 발송 로직 수정 중
    
    * feat: 일자리신문 구조 수정
    
    * fix: sqs anem
    
    * feat: 일자리신문 개선
    
    * ci/cd: deploy feature
    
    * ci/cd: fix
    
    * test
    
    * fix
    
    * ci/cd: fix newspaper slack url
    
    * start-send
    
    * fix: iam policy
    
    * Update start_send_newspaper_job.rb
    
    * fix
    
    * cut: legacy
    
    * ci/cd: edit env
    
    * fix: push_token fix
    
    * fix:link
    
    * Update prepare_service.rb
    
    ---------
    
    Co-authored-by: hanwool95 <hanwool95@snu.ac.kr>

--- app/services/newspaper/prepare_service.rb
+++ app/services/newspaper/prepare_service.rb
@@ -20,12 +20,17 @@ class Newspaper::PrepareService
       @users.find_in_batches(
         batch_size: @batch
       ).each_with_index do |batch, index|
-        batch.each_slice(500) do |slice|
-          Newspaper
-            .insert_all(
-              slice.map { |user| { date: @date, group: index, status: 'pending', user_id: user.id, push_token: user.push_token } }
-            )
+        begin
+          batch.each_slice(500) do |slice|
+            Newspaper
+              .insert_all(
+                slice.map { |user| { date: @date, group: index, status: 'pending', user_id: user.id, push_token: user.push_token.token } }
+              )
+          rescue => e
+            Jets.logger.error e
+          end
         end
+
       end
 
       log end_message
