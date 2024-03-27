f860a4df97f511b5b8f727310d6afd4b7b7e3038
JeongHaMin <wjdgkals74@gmail.com>
2024-03-27 11:10:24 +0900

    Update create_scheduled_message_job.rb

--- app/jobs/create_scheduled_message_job.rb
+++ app/jobs/create_scheduled_message_job.rb
@@ -4,12 +4,14 @@ class CreateScheduledMessageJob < ApplicationJob
   cron "0 20 ? * SUN *"
 
   def create_news_paper_mon_message
-    CreateNewsPaperActivelyCommonMessageService.call
+    CreateNewsPaperActivelyCommonMessageService.call if Jets.env == 'production'
+    CreateNewsPaperActivelyCommonMessageService.test_call if Jets.env != 'production'
   end
 
   cron "0 20 ? * WED *"
 
   def create_news_paper_thu_message
-    CreateNewsPaperActivelyCommonMessageService.call
+    CreateNewsPaperActivelyCommonMessageService.call if Jets.env == 'production'
+    CreateNewsPaperActivelyCommonMessageService.test_call if Jets.env != 'production'
   end
 end
