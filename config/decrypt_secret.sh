9730bcd7ee95191f0c1ba2b8d265611a09088b99
JeongHaMin <wjdgkals74@gmail.com>
2024-04-27 14:48:05 +0900

    FIX : json, format

--- config/decrypt_secret.sh
+++ config/decrypt_secret.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="bosalpim" \
---output ./config/FB_ADMIN_JSON.json ./config/fb_admin.json.gpg
+--output ./config/FB_ADMIN_JSON.json ./config/FB_ADMIN_JSON.json.gpg
