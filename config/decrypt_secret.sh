d9021440f6b0211ae6bc500f424b26984396a7c7
JeongHaMin <wjdgkals74@gmail.com>
2024-04-27 15:02:49 +0900

    Update decrypt_secret.sh

--- config/decrypt_secret.sh
+++ config/decrypt_secret.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gpg --quiet --batch --yes --decrypt --passphrase="bosalpim" \
+gpg --quiet --batch --yes --decrypt --passphrase="$FB_ADMIN_JSON_SECRET_PASSPHRASE" \
 --output ./config/FB_ADMIN_JSON.json ./config/FB_ADMIN_JSON.json.gpg
