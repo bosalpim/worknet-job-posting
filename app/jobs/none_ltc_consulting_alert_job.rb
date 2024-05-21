edb71eb7965009565205291d04f9469464cd785d
JeongHaMin <wjdgkals74@gmail.com>
2024-05-21 14:34:30 +0900

    Update none_ltc_consulting_alert_job.rb (#574)

--- app/jobs/none_ltc_consulting_alert_job.rb
+++ app/jobs/none_ltc_consulting_alert_job.rb
@@ -44,6 +44,8 @@ class NoneLtcConsultingAlertJob < ApplicationJob
   def before_consulting_list
     # created_at 기준으로 오늘 이전날짜 before_consulting + 오늘 상담 요청건들
     NoneLtcServiceRequest
+      .where.not(service_plan: %w[few_days once])
+      .or(NoneLtcServiceRequest.where(service_plan: nil))
       .where(status: 'before_consulting')
       .before_end_of_today
       .order(
@@ -57,7 +59,10 @@ class NoneLtcConsultingAlertJob < ApplicationJob
             .before_end_of_today
             .where.not(status: 'finish')
             .pluck(:none_ltc_service_request_id)
+
     NoneLtcServiceRequest
+      .where.not(service_plan: %w[few_days once])
+      .or(NoneLtcServiceRequest.where(service_plan: nil))
       .where(id: ids)
       .order(
         "created_at desc"
