5da42cac2c1e757fdfee01aa9409d861c2d080c5
hanwool95 <hanwool95@snu.ac.kr>
2024-06-14 16:36:34 +0900

    REFACTOR: 서비스를 하나로 추상화

--- app/services/job_support_project/submit_remind_service.rb
+++ app/services/job_support_project/submit_remind_service.rb
@@ -1,9 +1,12 @@
-class JobSupportProject::FirstSubmitRemindService
-  def initialize
+class JobSupportProject::SubmitRemindService
+  def initialize(standard_day_ago, title, due_day)
     # DB가 UTC 시간을 사용하고 있으므로 한국 시간(KST)을 기준으로 변경
-    kst_now = Time.now.in_time_zone('Asia/Seoul')
-    start_time = (kst_now - 2.days).beginning_of_day
-    end_time = (kst_now - 1.days).beginning_of_day
+    @kst_now = Time.now.in_time_zone('Asia/Seoul')
+    start_time = (@kst_now - standard_day_ago.days).beginning_of_day
+    end_time = (@kst_now - (standard_day_ago - 1).days).beginning_of_day
+
+    @title = title
+    @due_day = due_day
 
     @job_support_project_participants = JobSupportProjectParticipant
                                           .where('created_at >= ? AND created_at < ?', start_time.utc, end_time.utc)
@@ -15,15 +18,15 @@ class JobSupportProject::FirstSubmitRemindService
     Jets.logger.info "-------------- FirstSubmitRemind START --------------\n"
     @job_support_project_participants.each do |job_support_project_participant|
       Jets.logger.info "-------------- INFO START --------------\n"
-      Jets.logger.info "#{job_support_project_participant.id} 대상 1차 리마인드 발송\n"
+      Jets.logger.info "#{job_support_project_participant.id} 대상 리마인드 발송\n"
 
       business_registration = job_support_project_participant.job_posting.business.business_registration
       user_name = job_support_project_participant.user.name
-      today_date = Time.now.in_time_zone('Asia/Seoul').strftime('%m.%d')
+      due_date = (@kst_now + @due_day.days).in_time_zone('Asia/Seoul').strftime('%m.%d')
 
       if business_registration.nil?
         Jets.logger.info "사업자 등록증 없는 경우"
-        message = "안녕하세요 케어파트너입니다. 채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.
+        message = "#{@title}
 
 [제출 서류]
 1. 고유번호증 또는 사업자등록증 사업 참여 첫 1회만 제출
@@ -34,11 +37,11 @@ class JobSupportProject::FirstSubmitRemindService
 ■ 1588-5877로 문자 제출
 ■ Fax : 07080157158
 
-제출 기한: #{today_date}까지"
+제출 기한: #{due_date}까지"
         Notification::Lms.new(phone_number: job_support_project_participant.job_posting.manager_phone_number, message: message).send
       else
         Jets.logger.info "사업자 등록증 있는 경우\n"
-        message = "안녕하세요 케어파트너입니다. 채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.
+        message = "#{@title}
 
 [제출 서류]
 ■ #{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)
@@ -48,7 +51,7 @@ class JobSupportProject::FirstSubmitRemindService
 ■ 1588-5877로 문자 제출
 ■ Fax : 07080157158
 
-제출 기한: #{today_date}까지"
+제출 기한: #{due_date}까지"
         Notification::Lms.new(phone_number: job_support_project_participant.job_posting.manager_phone_number, message: message).send
       end
       Jets.logger.info "-------------- INFO END --------------\n"
