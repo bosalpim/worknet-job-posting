df8ac837f21d190f95a4dda2bb5fe2d71831a86b
hanwool95 <hanwool95@snu.ac.kr>
2024-06-14 16:14:29 +0900

    FEATURE: 문자 메세지 내용 수정

--- app/services/job_support_project/first_submit_remind_service.rb
+++ app/services/job_support_project/first_submit_remind_service.rb
@@ -25,41 +25,30 @@ class JobSupportProject::FirstSubmitRemindService
         Jets.logger.info "사업자 등록증 없는 경우"
         message = "안녕하세요 케어파트너입니다. 채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.
 
-          [제출 서류]
+[제출 서류]
+1. 고유번호증 또는 사업자등록증 사업 참여 첫 1회만 제출
+2. #{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)
 
-          고유번호증 또는 사업자등록증 사업 참여 첫 1회만 제출
+[서류 제출 방법]
+아래 방법 중 하나로 제출 해주세요
+■ 1588-5877로 문자 제출
+■ Fax : 07080157158
 
-          **#{user_name}**님 근로계약서 (주소, 주민등록번호 전체 포함)
-
-          [서류 제출 방법]
-
-          아래 방법 중 하나로 제출 해주세요
-
-          1588-5877로 문자 제출
-
-          Fax : 07080157158
-
-          제출 기한: **#{today_date}**까지
-        "
+제출 기한: #{today_date}까지"
         Notification::Lms.new(phone_number: job_support_project_participant.job_posting.manager_phone_number, message: message).send
       else
         Jets.logger.info "사업자 등록증 있는 경우\n"
         message = "안녕하세요 케어파트너입니다. 채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.
 
-          [제출 서류]
+[제출 서류]
+■ #{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)
 
-          **#{user_name}**님 근로계약서 (주소, 주민등록번호 전체 포함)
+[서류 제출 방법]
+아래 방법 중 하나로 제출 해주세요
+■ 1588-5877로 문자 제출
+■ Fax : 07080157158
 
-          [서류 제출 방법]
-
-          아래 방법 중 하나로 제출 해주세요
-
-          1588-5877로 문자 제출
-
-          Fax : 07080157158
-
-          제출 기한: **#{today_date}**까지
-        "
+제출 기한: #{today_date}까지"
         Notification::Lms.new(phone_number: job_support_project_participant.job_posting.manager_phone_number, message: message).send
       end
       Jets.logger.info "-------------- INFO END --------------\n"
