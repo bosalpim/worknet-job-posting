031dd34a03bb5a3a5072706aae3750ec2740e656
hanwool95 <hanwool95@snu.ac.kr>
2024-06-14 12:56:11 +0900

    TEST for creating new job

--- app/jobs/notification_service_job.rb
+++ app/jobs/notification_service_job.rb
@@ -44,7 +44,7 @@ class NotificationServiceJob < ApplicationJob
   cron "0 4 ? * * *"
 
   def target_job_posting_ads_after_posting_3days
-    process( { message_template_id: MessageTemplateName::TARGET_JOB_POSTING_AD_2 })
+    puts "happy day"
   end
 
   private
