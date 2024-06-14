4163e5fdd7424fbd005cb688b87f64e8da0f47ff
hanwool95 <hanwool95@snu.ac.kr>
2024-06-14 14:18:48 +0900

    TEST for change name

--- app/jobs/notification_service_job.rb
+++ app/jobs/notification_service_job.rb
@@ -44,8 +44,8 @@ class NotificationServiceJob < ApplicationJob
 
   cron "0 4 ? * * *"
 
-  def target_job_posting_ads_after_posting_3days
-    puts "happy day"
+  def target_job_posting_ads_after_upload_posting_3days
+    process( { message_template_id: MessageTemplates::TEMPLATES[MessageNames::TARGET_JOB_POSTING_AD_2] })
   end
 
   private
