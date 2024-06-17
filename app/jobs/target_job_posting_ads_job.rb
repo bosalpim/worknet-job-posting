2166442861ddc9d6e046baff85f26867fa7dd330
hanwool95 <hanwool95@snu.ac.kr>
2024-06-17 11:48:39 +0900

    TEST for other alimtalk

--- app/jobs/target_job_posting_ads_job.rb
+++ app/jobs/target_job_posting_ads_job.rb
@@ -3,14 +3,15 @@ class TargetJobPostingAdsJob < ApplicationJob
 
   cron "0 4 ? * * *"
   def target_job_posting_ads_after_posting_3days
-    begin
-      Jets.logger.info event
-
-      notification = Notification::FactoryService.create(MessageTemplates::TEMPLATES[MessageNames::TARGET_JOB_POSTING_AD_2], nil)
-      notification.process
-    rescue => e
-      Jets.logger.info e.full_message
-    end
+    JobSupportProject::SubmitRemindService.new(3, '안녕하세요 케어파트너입니다. 채용지원금 신청 서류 제출 일자가 지나 다시 연락드렸습니다.', 1).call
+    # begin
+    #   Jets.logger.info event
+    #
+    #   notification = Notification::FactoryService.create(MessageTemplates::TEMPLATES[MessageNames::TARGET_JOB_POSTING_AD_2], nil)
+    #   notification.process
+    # rescue => e
+    #   Jets.logger.info e.full_message
+    # end
 
   end
 
