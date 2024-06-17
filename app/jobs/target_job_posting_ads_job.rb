076cad236b7b28d6a89bc9d0f7bbb3a89cdf2240
hanwool95 <hanwool95@snu.ac.kr>
2024-06-17 12:00:21 +0900

    TEST for original job

--- app/jobs/target_job_posting_ads_job.rb
+++ app/jobs/target_job_posting_ads_job.rb
@@ -3,15 +3,14 @@ class TargetJobPostingAdsJob < ApplicationJob
 
   cron "0 4 ? * * *"
   def target_posting_ads_after_3days
-    JobSupportProject::SubmitRemindService.new(3, '안녕하세요 케어파트너입니다. 채용지원금 신청 서류 제출 일자가 지나 다시 연락드렸습니다.', 1).call
-    # begin
-    #   Jets.logger.info event
-    #
-    #   notification = Notification::FactoryService.create(MessageTemplates::TEMPLATES[MessageNames::TARGET_JOB_POSTING_AD_2], nil)
-    #   notification.process
-    # rescue => e
-    #   Jets.logger.info e.full_message
-    # end
+    begin
+      Jets.logger.info event
+
+      notification = Notification::FactoryService.create(MessageTemplates::TEMPLATES[MessageNames::TARGET_JOB_POSTING_AD_2], nil)
+      notification.process
+    rescue => e
+      Jets.logger.info e.full_message
+    end
 
   end
 
