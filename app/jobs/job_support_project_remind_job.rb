d2f55a12f34eec17a382d960b8ce97dd0f793268
hanwool95 <hanwool95@snu.ac.kr>
2024-06-18 19:29:12 +0900

    FEATURE: 문구 변경

--- app/jobs/job_support_project_remind_job.rb
+++ app/jobs/job_support_project_remind_job.rb
@@ -2,12 +2,12 @@ class JobSupportProjectRemindJob < ApplicationJob
 
   cron "0 0 ? * * *"
   def first_submit_remind
-    JobSupportProject::SubmitRemindService.new(2, '안녕하세요 케어파트너입니다. 채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.', 0).call
+    JobSupportProject::SubmitRemindService.new(2, '채용 지원금 서류 제출 기한이 오늘까지 입니다. 확인 후 제출해주세요.', 0).call
   end
 
   cron "0 4 ? * * *"
   def second_submit_remind
-    JobSupportProject::SubmitRemindService.new(3, '안녕하세요 케어파트너입니다. 채용지원금 신청 서류 제출 일자가 지나 다시 연락드렸습니다.', 0).call
+    JobSupportProject::SubmitRemindService.new(3, '채용지원금 신청 서류 제출 일자가 지나 다시 연락드렸습니다.', 0).call
   end
 
 end
