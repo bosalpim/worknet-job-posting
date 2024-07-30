653741d16f59277d7fb0a5499750d15859342ada
JeongHaMin <wjdgkals74@gmail.com>
2024-07-30 17:49:52 +0900

    FIX : 금요일에 추가로 발송되는 전환 유도 CRM만  나가도록 수정 (#622)

--- app/jobs/draft_conversion_induce_msg_job.rb
+++ app/jobs/draft_conversion_induce_msg_job.rb
@@ -1,22 +1,22 @@
 class DraftConversionInduceMsgJob < ApplicationJob
   include AlimtalkMessage
 
-  cron "0 7 ? * * *"
+  cron "0 7 ? * MON-THU,SAT-SUN *"
   def first_day_except_address
     DraftConversionMessageService.call(MessageTemplates[MessageNames::ONE_DAY_CAREPARTNER_DRAFT_CRM])
   end
 
-  cron "0 7 ? * * *"
+  cron "0 7 ? * MON-THU,SAT-SUN *"
   def first_day_only_address
     DraftConversionMessageService.call(MessageTemplates[MessageNames::ONE_DAY_CAREPARTNER_ADDRESS_LEAK_CRM])
   end
 
-  cron "0 7 ? * * *"
+  cron "0 7 ? * MON-THU,SAT-SUN *"
   def second_day_except_address
     DraftConversionMessageService.call(MessageTemplates[MessageNames::TWO_DAY_CAREPARTNER_DRAFT_CRM])
   end
 
-  cron "0 7 ? * * *"
+  cron "0 7 ? * MON-THU,SAT-SUN *"
   def check_certification
     DraftConversionMessageService.call(MessageTemplateName::CERTIFICATION_UPDATE)
   end
