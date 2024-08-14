ac09e6f8ebf9b679088066ad0b31473bf2129c01
hanwool95 <hanwool95@snu.ac.kr>
2024-08-14 18:21:22 +0900

    Test for create new job

--- app/jobs/create_newspaper_job.rb
+++ app/jobs/create_newspaper_job.rb
@@ -16,6 +16,21 @@ class CreateNewspaperJob < ApplicationJob
     end
   end
 
+  cron "0 20 ? * TUE *"
+
+  def create_tuesday_newspaper
+    if Jets.env.production?
+      Newspaper::PrepareService.new(
+        date: DateTime.now,
+        ).call
+    elsif Jets.env.staging?
+      Newspaper::PrepareService.new(
+        date: DateTime.now,
+        batch: 2
+      ).call
+    end
+  end
+
   cron "0 20 ? * WED *"
 
   def create_thursday_newspaper
