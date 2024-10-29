190723c72098a0e2dbfa694fa2fa54d6dc35b556
JeongHaMin <wjdgkals74@gmail.com>
2024-10-29 13:03:35 +0900

    Update submit_remind_service.rb

--- app/services/job_support_project/submit_remind_service.rb
+++ app/services/job_support_project/submit_remind_service.rb
@@ -12,16 +12,15 @@ class JobSupportProject::SubmitRemindService
     current_year_start = Time.now.beginning_of_year
     current_year_end = Time.now.end_of_year
 
-    @job_posting_results = JobPostingResult.where(id: 38)
-    # @job_posting_results = JobPostingResult
-    #                          .where('job_posting_results.created_at >= ? AND job_posting_results.created_at < ?', start_time.utc, end_time.utc)
-    #                          .where(result_type: 'success')
-    #                          .left_joins(:user)
-    #                          .where('job_posting_results.user_id IS NULL OR users.birth_year <= ?', Time.now.year - 60)
-    #                          .left_joins(job_posting: :job_support_project_participants)
-    #                          .where('job_support_project_participants.id IS NULL OR job_support_project_participants.is_done = ?', false)
-    #                          .joins("LEFT JOIN job_support_project_participants AS jsp ON job_posting_results.user_id = jsp.user_id AND jsp.is_done = TRUE AND jsp.created_at BETWEEN '#{current_year_start.utc}' AND '#{current_year_end.utc}'")
-    #                          .where('jsp.id IS NULL')
+    @job_posting_results = JobPostingResult
+                             .where('job_posting_results.created_at >= ? AND job_posting_results.created_at < ?', start_time.utc, end_time.utc)
+                             .where(result_type: 'success')
+                             .left_joins(:user)
+                             .where('job_posting_results.user_id IS NULL OR users.birth_year <= ?', Time.now.year - 60)
+                             .left_joins(job_posting: :job_support_project_participants)
+                             .where('job_support_project_participants.id IS NULL OR job_support_project_participants.is_done = ?', false)
+                             .joins("LEFT JOIN job_support_project_participants AS jsp ON job_posting_results.user_id = jsp.user_id AND jsp.is_done = TRUE AND jsp.created_at BETWEEN '#{current_year_start.utc}' AND '#{current_year_end.utc}'")
+                             .where('jsp.id IS NULL')
   end
 
   def call
@@ -29,13 +28,13 @@ class JobSupportProject::SubmitRemindService
       business_registration = job_posting_result.job_posting.business.business_registration
       user_name = job_posting_result.user.name
       due_date = (@kst_now + @due_day.days).in_time_zone('Asia/Seoul').strftime('%m.%d')
-      sms_body = "다음 문서들을 해당 번호로 보내주세요.\n\n#{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)"
+      sms_body = "다음 문서들을 해당 번호로 보내주세요. 1. #{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)"
       message = "[채용 지원금 서류 제출 알림]\n\n안녕하세요 #{job_posting_result.job_posting.business.name} 담당자님 케어파트너입니다.\n#{@title}\n\n[제출 서류]"
-      if business_registration.present?
-        message += "\n■ 고유번호증 또는 사업자등록증 사업 참여 첫 1회만 제출"
-        sms_body += "\n고유번호증 또는 사업자등록증"
+      if business_registration.nil?
+        message += "\n■ 센터의 고유번호증 또는 사업자등록증 (사업 참여 첫 1회만 제출)"
+        sms_body += "2. 고유번호증 또는 사업자등록증"
       end
-      message += "\n#{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)"
+      message += "\n■ #{user_name}님 근로계약서 (주소, 주민등록번호 전체 포함)"
       message += "\n\n[서류 제출 방법]\n아래 방법 중 하나로 제출 해주세요 (세 가지 중 택 1)\n■ 바로 제출하기 > 하단 \"바로 제출하기\" 버튼을 눌러서 서류를 제출헤주세요.\n■ 문자로 제출하기 > 하단 \"문자로 제출하기\" 버튼을 눌러서 서류를 제출해주세요.\n■ Fax > 07080157158로 서류를 제출해주세요.\n\n제출 기한: #{due_date}까지"
 
       body = {
