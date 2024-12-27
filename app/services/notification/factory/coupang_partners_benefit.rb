7fc35eed768e5c37f9f8e4a6234cc084950bc577
hanwool95 <hanwool95@snu.ac.kr>
2024-12-27 14:12:25 +0900

    앱푸시 발송 최적화. 쿠팡파트너스 푸시 로깅 배치화 & 푸시 배치 크기 향상 (#714)
    * feat: send batch log data
    
    * feat: add custumizing batch size
    
    * fix: app push in production

--- app/services/notification/factory/coupang_partners_benefit.rb
+++ app/services/notification/factory/coupang_partners_benefit.rb
@@ -8,30 +8,36 @@ class Notification::Factory::CoupangPartnersBenefit < Notification::Factory::Not
                 .where(alerts: { name: 'coupang_partners' })
                 .distinct
 
+    @batch_size = 20
+
     create_message
+    send_log_batches
   end
 
   def create_message
-    @list.each do |user |
+    @log_data = []
 
+    @list.each do |user|
       base_path = "benefit/button-press"
 
       user_push_token = user.user_push_tokens.first&.token
 
       if user_push_token&.present?
         link = "#{base_path}?utm_source=message&utm_medium=#{NOTIFICATION_TYPE_APP_PUSH}&utm_campaign=button-press-alert&referral=app_push"
-        AmplitudeService.instance.log_array([{
-                                               "user_id" => user.public_id,
-                                               "event_type" => "[Action] Receive Notification",
-                                               "event_properties" => {
-                                                 "template" => MessageTemplateName::COUPANG_PARTNERS_BENEFIT,
-                                                 "sender_type" => SENDER_TYPE_CAREPARTNER,
-                                                 "receiver_type" => RECEIVER_TYPE_BUSINESS,
-                                                 "send_at" => Time.current + (9 * 60 * 60),
-                                                 "notiName" => "benefit_buttonclick",
-                                                 "itemId" => "coupang partners"
-                                               }
-                                             }])
+
+        @log_data << {
+          "user_id" => user.public_id,
+          "event_type" => "[Action] Receive Notification",
+          "event_properties" => {
+            "template" => MessageTemplateName::COUPANG_PARTNERS_BENEFIT,
+            "sender_type" => SENDER_TYPE_CAREPARTNER,
+            "receiver_type" => RECEIVER_TYPE_BUSINESS,
+            "send_at" => Time.current + (9 * 60 * 60),
+            "notiName" => "benefit_buttonclick",
+            "itemId" => "coupang partners"
+          }
+        }
+
         if Jets.env.production?
           @app_push_list.push(
             AppPush.new(
@@ -54,6 +60,15 @@ class Notification::Factory::CoupangPartnersBenefit < Notification::Factory::Not
           )
         end
       end
+    end
+  end
+
+  def send_log_batches
+    return if @log_data.empty?
+
+    batch_size = 2000
+    @log_data.each_slice(batch_size) do |batch|
+      AmplitudeService.instance.log_array(batch)
     end
   end
 end
