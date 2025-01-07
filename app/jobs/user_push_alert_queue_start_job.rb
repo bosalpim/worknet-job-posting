6b870f0733bbe2d1408ffe8412300d9b48156042
hanwool95 <hanwool95@snu.ac.kr>
2025-01-07 20:53:30 +0900

    chore: change to original time and group

--- app/jobs/user_push_alert_queue_start_job.rb
+++ app/jobs/user_push_alert_queue_start_job.rb
@@ -7,7 +7,7 @@ class UserPushAlertQueueStartJob < ApplicationJob
   # 메세지 보내는 시간에 맞춰서 실행합니다.
 
   iam_policy 'sqs'
-  cron "40 10 * * ? *"
+  cron "0 9 * * ? *"
   def start_send_coupang_partners
     start_send_sqs("coupang_partners")
   end
@@ -19,14 +19,14 @@ class UserPushAlertQueueStartJob < ApplicationJob
   end
 
   iam_policy 'sqs'
-  cron "50 10 * * ? *"
+  cron "0 10 * * ? *"
   def start_send_yoyang_run_push
     start_send_sqs("yoyang_run")
   end
 
   def start_send_sqs(alert_name)
     date = DateTime.now.at_beginning_of_day.strftime('%Y/%m/%d')
-    group = 1
+    group = 0
     sqs.send_message(
       queue_url: Main::USER_PUSH_JOB_QUEUE_URL,
       message_group_id: "push-#{alert_name}-#{date}",
