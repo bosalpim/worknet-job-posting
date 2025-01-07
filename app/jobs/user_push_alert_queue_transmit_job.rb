282ed78600b80b29672f835eac0c0fa5f63b3792
hanwool95 <hanwool95@snu.ac.kr>
2025-01-07 18:26:25 +0900

    hotfix: only execute

--- app/jobs/user_push_alert_queue_transmit_job.rb
+++ app/jobs/user_push_alert_queue_transmit_job.rb
@@ -7,7 +7,7 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
   sqs_event Jets.env.production? ? "user_push_job_queue.fifo" : "user_push_job_queue_stag.fifo"
 
   # queue에서 순차적으로 보내온 이벤트를 처리하는 job입니다
-  def process
+  def execute
     Jets.logger.info "#{JSON.dump(event)}"
 
     payload = sqs_event_payload
@@ -38,28 +38,6 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
 
     Jets.logger.info "Alert=#{alert_name} [DATE=#{date}, GROUP=#{group}] #{user_push_queue.processing.length}건 발송 시작"
 
-    send_push(alert_name, user_push_queue)
-
-    Jets.logger.info "[Alert=#{alert_name} DATE=#{date}, GROUP=#{group}] #{user_push_queue.processing.length}건 발송 종료"
-
-    updated_count = user_push_queue.processing.update_all(status: 'done')
-
-    Jets.logger.info "[Alert=#{alert_name} DATE=#{date}, GROUP=#{group}] #{updated_count}건 처리 완료"
-
-    next_group = group + 1
-    sqs.send_message(
-      queue_url: Main::USER_PUSH_JOB_QUEUE_URL,
-      message_group_id: "#{alert_name}-#{date}",
-      message_deduplication_id: "#{alert_name}-#{date}-#{next_group}",
-      message_body: JSON.dump({
-                                alert_name: alert_name,
-                                date: date,
-                                group: next_group
-                              })
-    )
-  end
-
-  def send_push(alert_name, user_push_queue)
     case alert_name
     when "coupang_partners"
       factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
@@ -85,5 +63,51 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
     end
     factory.notify
     factory.save_result
+
+    Jets.logger.info "[Alert=#{alert_name} DATE=#{date}, GROUP=#{group}] #{user_push_queue.processing.length}건 발송 종료"
+
+    updated_count = user_push_queue.processing.update_all(status: 'done')
+
+    Jets.logger.info "[Alert=#{alert_name} DATE=#{date}, GROUP=#{group}] #{updated_count}건 처리 완료"
+
+    next_group = group + 1
+    sqs.send_message(
+      queue_url: Main::USER_PUSH_JOB_QUEUE_URL,
+      message_group_id: "#{alert_name}-#{date}",
+      message_deduplication_id: "#{alert_name}-#{date}-#{next_group}",
+      message_body: JSON.dump({
+                                alert_name: alert_name,
+                                date: date,
+                                group: next_group
+                              })
+    )
   end
+
+  # def send_push(alert_name, user_push_queue)
+  #   case alert_name
+  #   when "coupang_partners"
+  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+  #                                                        base_path = "/benefit/button-press",
+  #                                                        title = "버튼 누르고 10원 받기 알림💎",
+  #                                                        body = "지금 바로 포인트 10원 받을 수 있어요",
+  #                                                        campaign_name = "button-press-alert")
+  #   when "quiz_5"
+  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+  #                                                        base_path = "/quiz/daily-proverbs",
+  #                                                        title = "💡퀴즈 풀고 15원 받기 알림💡",
+  #                                                        body = "지금 우리말 속담을 맞춰보세요",
+  #                                                        campaign_name = "quiz-5-alert")
+  #   when "yoyang_run"
+  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+  #                                                        base_path = "/benefit/games/yoyang-run",
+  #                                                        title = "🐱 게임하고 포인트 무제한 받기",
+  #                                                        body = "지금 달려라 요양이 게임 한판 해보세요",
+  #                                                        campaign_name = "yoyang-run-alert")
+  #   else
+  #     Jets.logger.info "alert Name not found"
+  #     return
+  #   end
+  #   factory.notify
+  #   factory.save_result
+  # end
 end
