4f0e12bce3ce51c047902ec74619f3f57f20212c
hanwool95 <hanwool95@snu.ac.kr>
2025-01-08 10:04:34 +0900

    refactor: add function of sending push

--- app/jobs/user_push_alert_queue_transmit_job.rb
+++ app/jobs/user_push_alert_queue_transmit_job.rb
@@ -3,6 +3,34 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
   include MessageTemplateName
   include KakaoNotificationLoggingHelper
 
+  def send_push(alert_name, user_push_queue)
+    case alert_name
+    when "coupang_partners"
+      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+                                                         base_path = "/benefit/button-press",
+                                                         title = "버튼 누르고 10원 받기 알림💎",
+                                                         body = "지금 바로 포인트 10원 받을 수 있어요",
+                                                         campaign_name = "button-press-alert")
+    when "quiz_5"
+      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+                                                         base_path = "/quiz/daily-proverbs",
+                                                         title = "💡퀴즈 풀고 15원 받기 알림💡",
+                                                         body = "지금 우리말 속담을 맞춰보세요",
+                                                         campaign_name = "quiz-5-alert")
+    when "yoyang_run"
+      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
+                                                         base_path = "/benefit/games/yoyang-run",
+                                                         title = "🐱 게임하고 포인트 무제한 받기",
+                                                         body = "지금 달려라 요양이 게임 한판 해보세요",
+                                                         campaign_name = "yoyang-run-alert")
+    else
+      Jets.logger.info "alert Name not found"
+      return
+    end
+    factory.notify
+    factory.save_result
+  end
+
   iam_policy 'sqs'
   sqs_event Jets.env.production? ? "user_push_job_queue.fifo" : "user_push_job_queue_stag.fifo"
 
@@ -38,31 +66,7 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
 
     Jets.logger.info "Alert=#{alert_name} [DATE=#{date}, GROUP=#{group}] #{user_push_queue.processing.length}건 발송 시작"
 
-    case alert_name
-    when "coupang_partners"
-      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-                                                         base_path = "/benefit/button-press",
-                                                         title = "버튼 누르고 10원 받기 알림💎",
-                                                         body = "지금 바로 포인트 10원 받을 수 있어요",
-                                                         campaign_name = "button-press-alert")
-    when "quiz_5"
-      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-                                                         base_path = "/quiz/daily-proverbs",
-                                                         title = "💡퀴즈 풀고 15원 받기 알림💡",
-                                                         body = "지금 우리말 속담을 맞춰보세요",
-                                                         campaign_name = "quiz-5-alert")
-    when "yoyang_run"
-      factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-                                                         base_path = "/benefit/games/yoyang-run",
-                                                         title = "🐱 게임하고 포인트 무제한 받기",
-                                                         body = "지금 달려라 요양이 게임 한판 해보세요",
-                                                         campaign_name = "yoyang-run-alert")
-    else
-      Jets.logger.info "alert Name not found"
-      return
-    end
-    factory.notify
-    factory.save_result
+    send_push(alert_name, user_push_queue)
 
     Jets.logger.info "[Alert=#{alert_name} DATE=#{date}, GROUP=#{group}] #{user_push_queue.processing.length}건 발송 종료"
 
@@ -83,31 +87,4 @@ class UserPushAlertQueueTransmitJob < ApplicationJob
     )
   end
 
-  # def send_push(alert_name, user_push_queue)
-  #   case alert_name
-  #   when "coupang_partners"
-  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-  #                                                        base_path = "/benefit/button-press",
-  #                                                        title = "버튼 누르고 10원 받기 알림💎",
-  #                                                        body = "지금 바로 포인트 10원 받을 수 있어요",
-  #                                                        campaign_name = "button-press-alert")
-  #   when "quiz_5"
-  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-  #                                                        base_path = "/quiz/daily-proverbs",
-  #                                                        title = "💡퀴즈 풀고 15원 받기 알림💡",
-  #                                                        body = "지금 우리말 속담을 맞춰보세요",
-  #                                                        campaign_name = "quiz-5-alert")
-  #   when "yoyang_run"
-  #     factory = Notification::Factory::UserPushAlert.new(user_push_queue.processing,
-  #                                                        base_path = "/benefit/games/yoyang-run",
-  #                                                        title = "🐱 게임하고 포인트 무제한 받기",
-  #                                                        body = "지금 달려라 요양이 게임 한판 해보세요",
-  #                                                        campaign_name = "yoyang-run-alert")
-  #   else
-  #     Jets.logger.info "alert Name not found"
-  #     return
-  #   end
-  #   factory.notify
-  #   factory.save_result
-  # end
 end
