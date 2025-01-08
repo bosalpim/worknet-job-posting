d18365c617710c66ccab3a0750be1b4a9b6ad33e
hanwool95 <hanwool95@snu.ac.kr>
2025-01-08 10:21:55 +0900

    Revert "chore: test for sending push by function"
    This reverts commit f1dc6b979626d1baee98babcf99236eeb48470a9.

--- app/jobs/user_push_alert_queue_prepare_job.rb
+++ app/jobs/user_push_alert_queue_prepare_job.rb
@@ -5,7 +5,7 @@ class UserPushAlertQueuePrepareJob < ApplicationJob
   # user_push_alert_queues table에 전송을 위한 데이터를 넣는 사전 준비 job입니다.
   # 메세지 보내는 시간 최소 15분 전에 실행해야 합니다.
 
-  cron "15 1 * * ? *"
+  cron "40 8 * * ? *"
   def coupang_partners_push_alert
     prepare_user_push_alert("coupang_partners")
   end
