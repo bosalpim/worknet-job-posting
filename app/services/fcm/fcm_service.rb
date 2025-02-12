0ba7a33b532e480fc29317bac942f3d1f5b7e6a5
JeongHaMin <wjdgkals74@gmail.com>
2025-02-12 21:37:30 +0900

    feat add exception handle

--- app/services/fcm/fcm_service.rb
+++ app/services/fcm/fcm_service.rb
@@ -8,20 +8,32 @@ class Fcm::FcmService
   end
 
   def send_message(token, message)
-    response = self.class.post('/v1/projects/carepartner-app-v1/messages:send',
-                               headers: { 'Authorization' => "Bearer #{@auth_token}", 'Content-Type' => 'application/json' },
-                               body: message_body(token, message).to_json)
-
-    if response.code == 401 # Unauthorized, possibly due to expired token
-      authorize # Re-authorize and get a new token
-      response = self.class.post('/v1/projects/carepartner-app-v1/messages:send', # Retry the request with the new token
+    begin
+      response = self.class.post('/v1/projects/carepartner-app-v1/messages:send',
                                  headers: { 'Authorization' => "Bearer #{@auth_token}", 'Content-Type' => 'application/json' },
                                  body: message_body(token, message).to_json)
-    end
 
-    result = { success: response.code == 200 }
-    result["errorCode"] = response['error']['details'].first['errorCode'] if response.code != 200
-    result
+      if response.code == 401 # Unauthorized, possibly due to expired token
+        authorize # Re-authorize and get a new token
+        response = self.class.post('/v1/projects/carepartner-app-v1/messages:send', # Retry the request with the new token
+                                   headers: { 'Authorization' => "Bearer #{@auth_token}", 'Content-Type' => 'application/json' },
+                                   body: message_body(token, message).to_json)
+      end
+
+      result = { success: response.code == 200 }
+
+      # 예외 처리를 추가하여 nil 에러 방지
+      if response.code != 200
+        error_details = response.dig('error', 'details') # 안전한 접근 방식
+        result["errorCode"] = error_details.first['errorCode'] if error_details.is_a?(Array) && error_details.any?
+      end
+
+      result
+    rescue StandardError => e
+      # 예외 발생 시 안전한 결과 반환
+      Jets.logger.info "Token: #{token}, Body: #{message}"
+      { success: false, errorCode: "UNKNWON" }
+    end
   end
 
   private
