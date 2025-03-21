8b058e2f69b8f0a8c3ae0cc900ed859fde7f3085
JeongHaMin <wjdgkals74@gmail.com>
2025-03-21 18:15:56 +0900

    feat 신규일자리 알림톡 일자리 확인하기 앱링크 전환 실험 정리 (#797)
    * feat 신규일자리 알림톡 일자리 확인하기 앱링크 전환 실험 정리
    
    * Update kakao_template_service.rb
    
    * chore unused code
    
    * Update target_user_job_posting_service.rb

--- app/modules/alimtalk_message.rb
+++ app/modules/alimtalk_message.rb
@@ -13,12 +13,11 @@ module AlimtalkMessage
     CBT_DRAFT_CRM = 'cbt_draft_crm'
     TARGET_JOB_BUSINESS_FREE_TRIALS = 'target_user_business_free_trials'
     CLOSE_JOB_POSTING_NOTIFICATION='close_job_posting_notification'
-    TARGET_USER_JOB_POSTING_WITH_APP_LINK = "target_user_job_posting_V7"
   end
 
   module MessageTemplates
     TEMPLATES = {
-      MessageNames::TARGET_USER_JOB_POSTING => "target_user_job_posting_V6",
+      MessageNames::TARGET_USER_JOB_POSTING => "target_user_job_posting_V7",
       MessageNames::ONE_DAY_CAREPARTNER_DRAFT_CRM => "high-salary-job",
       MessageNames::ONE_DAY_CAREPARTNER_ADDRESS_LEAK_CRM => 'enter-location-1',
       MessageNames::ONE_DAY_CAREPARTNER_CERTFICATION_LEAK_CRM => 'carepartner-present',
