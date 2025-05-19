22663f486e2e6dc34b93036dac4bedd7bbcc0aa2
80xer <80xer.s@gmail.com>
2025-05-19 11:15:33 +0900

    feat: test academy boost (#821)

--- app/jobs/user_push_alert_queue_prepare_job.rb
+++ app/jobs/user_push_alert_queue_prepare_job.rb
@@ -39,7 +39,7 @@ class UserPushAlertQueuePrepareJob < ApplicationJob
     prepare_user_push_alert("coupang_roulette")
   end
 
-  cron "0 9 * * ? *"
+  cron "30 2 * * ? *"
   def prepare_academy_boost_alert
     query = User.joins(:user_push_tokens)
                   .joins("INNER JOIN academy_course_enrollments ON academy_course_enrollments.user_id = users.id")
